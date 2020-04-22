6cb22307f5050f3de4ab80563d903a2aafbcbb99
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-04-22 18:33:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const bot = new Discord.Client();
 
 const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU';
 
+const PREFIX = '?';
+
+bot.on('message', msg=>{
+    if(msg.content == "운지"){
+        msg.reply('예아!')
+    }
+})
+
 bot.login(process.env.token);
