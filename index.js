e4244cef35df96f083ddb625c3f6d7d8556ac1a0
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-04-22 18:24:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.Xp63Wg.K7rbVFpuH9G86URoC5HhOtlZBpw';
+const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU';
 
 bot.login(process.env.token);
