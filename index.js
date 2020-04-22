1dcaad7859178f724113085aa27edb34339c0734
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-04-22 18:23:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,4 @@ const bot = new Discord.Client();
 
 const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.Xp63Wg.K7rbVFpuH9G86URoC5HhOtlZBpw';
 
-bot.on('ready', () =>{
-    console.log('Im still alive!');
-})
-
-bot.on('message', msg=>{
-    if(msg.content == "?운지"){
-        msg.reply('예아!');
-    }
-})
-
 bot.login(process.env.token);
