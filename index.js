9098ade010d07716059744c06f80351e6f7a8441
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-15 04:04:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const bot = new Discord.Client();
 const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU';
 
 bot.on('message', msg=>{
-    if(msg.content == "?운지"){
+    if(msg.content == "?부엉이"){
         msg.reply('우흥!')
+    }
+
+})
+
+bot.on('message', msg=>{
+    if(msg.content == "?운지"){
+        msg.reply('저기 부엉이바위 쪽으로 가자')
     }
 })
 
