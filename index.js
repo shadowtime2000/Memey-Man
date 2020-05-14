9744b8b794bc58921458410bfaf2284691b52cb3
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-15 02:13:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU';
 
 bot.on('message', msg=>{
     if(msg.content == "?운지"){
-        msg.reply('예아!')
+        msg.reply('우흥!')
     }
 })
 
