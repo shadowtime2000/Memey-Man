e628d6f248b2272035f471062d9cf7f2eb9c1bcb
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-15 04:49:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,9 @@ const bot = new Discord.Client();
 
 const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU';
 
-bot.on('ready', () => {
-    bot.user.setStatus('available')
-    bot.user.setPresence({
-        game: {
-            name: 'MC무현 노래작업',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/mcmoohyun"
-        }
-    });
-})
+bot.user.setPresence({ game: { name: 'MC무현 라이브 공연' , type: 'WATCHING' }, status: 'online' })
+    .then(console.log)
+    .catch(console.error);
 
 bot.on('message', msg=>{
     if(msg.content == "?부엉이"){
