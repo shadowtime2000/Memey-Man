a1de03e6d3d27f03e93b8165021e5bdc6ffdc468
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-15 04:50:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const bot = new Discord.Client();
 
 const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU';
 
-bot.user.setPresence({ game: { name: 'MC무현 라이브 공연' , type: 'WATCHING' }, status: 'online' })
-    .then(console.log)
-    .catch(console.error);
-
 bot.on('message', msg=>{
     if(msg.content == "?부엉이"){
         msg.reply('우흥!')
