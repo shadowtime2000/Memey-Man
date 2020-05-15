f03d8adf054ba55e3c2e5118e4ad15899ec5f19c
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-15 09:10:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -40,4 +40,32 @@ bot.on('message', msg=>{
 
 })
 
+ if (message.content.startsWith("?kick")) {
+    // Easy way to get member object though mentions.
+    var member = message.mentions.members.first();
+    // Kick
+    member.kick().then((member) => {
+        // Successmessage
+        message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+    }).catch(() => {
+        // Failmessage
+        message.channel.send("Access Denied");
+    });
+}
+});
+client.on("message", (message) => {
+    if (message.content.startsWith("?ban")) {
+        // Easy way to get member object though mentions.
+        var member = message.mentions.members.first();
+        // ban
+        member.ban().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: ");
+        }).catch(() => {
+            // Failmessage
+            message.channel.send("Access Denied");
+        });
+    }
+});
+
 bot.login(process.env.token);
