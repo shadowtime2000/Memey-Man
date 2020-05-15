f45cc6399cf3471874127042f84057a65be04915
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-15 10:35:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on('message', msg=>{
     
     }
     
-    if (message.content.startsWith("?kick")) {
+    if(message.content.startsWith('?kick')) {
         // Easy way to get member object though mentions.
         var member= message.mentions.members.first();
         // Kick
@@ -51,7 +51,7 @@ bot.on('message', msg=>{
         });
     }
 
-    if (message.content.startsWith("?ban")) {
+    if(message.content.startsWith('?ban')) {
         // Easy way to get member object though mentions.
         var member= message.mentions.members.first();
         // ban
