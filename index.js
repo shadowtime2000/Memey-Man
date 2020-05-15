6413002e6b7baf73f3b3df47e3c233a13f880bcd
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-15 10:43:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -40,27 +40,27 @@ bot.on('message', msg=>{
     
     if(msg.content.startsWith('?kick')) {
         // Easy way to get member object though mentions.
-        var member= message.mentions.members.first();
+        var member= msg.mentions.members.first();
         // Kick
         member.kick().then((member) => {
             // Successmessage
-            message.channel.send("" + member.displayName + " 님을 부엉이바위 쪽으로 보냈습니다.");
+            msg.channel.send("" + member.displayName + " 님을 부엉이바위 쪽으로 보냈습니다.");
         }).catch(() => {
              // Failmessage
-            message.channel.send("실패했습니다.");
+            msg.channel.send("실패했습니다.");
         });
     }
 
     if(msg.content.startsWith('?ban')) {
         // Easy way to get member object though mentions.
-        var member= message.mentions.members.first();
+        var member= msg.mentions.members.first();
         // ban
         member.ban().then((member) => {
             // Successmessage
-            message.channel.send("" + member.displayName + " 님을 국정원 지하에 가뒀습니다. ");
+            msg.channel.send("" + member.displayName + " 님을 국정원 지하에 가뒀습니다. ");
         }).catch(() => {
              // Failmessage
-            message.channel.send("실패했습니다.");
+            msg.channel.send("실패했습니다.");
         });
     }
     
