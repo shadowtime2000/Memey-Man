6389d471a350bc3b0dc36760a3a96080305b20d0
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-15 11:33:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bot = new Discord.Client();
 
 const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU';
 
+const channel = client.channels.find('name', 챗)
+
 bot.once('ready', () => {
  console.log('Ready!');
 });
