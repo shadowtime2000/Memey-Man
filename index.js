9c03ad8208ff5211998e4f00a1568e4ab22a8a16
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-15 09:18:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -38,33 +38,34 @@ bot.on('message', msg=>{
     
     }
     
-    if (message.content.startsWith("?kick")) {
-    // Easy way to get member object though mentions.
-    var member = message.mentions.members.first();
-    // Kick
-    member.kick().then((member) => {
-        // Successmessage
-        message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
-    }).catch(() => {
-        // Failmessage
-        message.channel.send("Access Denied");
-    });
-
-bot.on("message", (message) => {
-    if (message.content.startsWith("?ban")) {
+     if (message.content.startsWith("$kick")) {
         // Easy way to get member object though mentions.
-        var member = message.mentions.members.first();
+        var member= message.mentions.members.first();
+        // Kick
+        member.kick().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+        }).catch(() => {
+             // Failmessage
+            message.channel.send("Access Denied");
+        });
+    }
+
+})
+
+ bot.on("message", (message) => {
+    if (message.content.startsWith("$ban")) {
+        // Easy way to get member object though mentions.
+        var member= message.mentions.members.first();
         // ban
         member.ban().then((member) => {
             // Successmessage
             message.channel.send(":wave: " + member.displayName + " has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: ");
         }).catch(() => {
-            // Failmessage
+             // Failmessage
             message.channel.send("Access Denied");
         });
     }
 });
-
-})
 
 bot.login(process.env.token);
