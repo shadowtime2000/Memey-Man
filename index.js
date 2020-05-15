a6cf438d9893ce33677f022c66dc2fdd93abbbf2
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-15 11:34:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 
 const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU';
 
-const channel = client.channels.find('name', 챗)
+const channel = bot.channels.find('name', 챗)
 
 bot.once('ready', () => {
  console.log('Ready!');
