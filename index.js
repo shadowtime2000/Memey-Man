0883687012c3910bb086a5096dbe5589badc6a23
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-17 04:19:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.user.setPresence('MCMH LIVE', { type: 'WATCHING' })
-  .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
-  .catch(console.error);
-
 const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU';
 
 bot.on("ready", () =>{
     console.log('Logged in!');
 });
+
+bot.user.setPresence('MCMH LIVE', { type: 'WATCHING' })
+  .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+  .catch(console.error);
 
 bot.on('message', msg=>{
     if(msg.content == "?부엉이"){
