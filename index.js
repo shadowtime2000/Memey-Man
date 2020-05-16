1dcdb3f905e5f60ccb3de857c7a9f7e31f6cca50
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-17 04:21:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bot = new Discord.Client();
 const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU';
 
 bot.on("ready", () =>{
+    bot.login("NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU")
     console.log('Logged in!');
 });
 
