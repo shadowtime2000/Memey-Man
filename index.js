5ebe6d948f31af0f2ace9163dde2b667d7071f93
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-22 11:26:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -80,11 +80,6 @@ bot.on('message', msg=>{
 	    
     }
 	
-   if(msg.content.includes('김대중')) {
-       msg.reply("그런말 하면 안된다!")
-       msg.delete (1000);
-   }
-	
     if(msg.content.startsWith('?찰과상')) {
 	var member= msg.mentions.members.first();
 	var facts = ["을(를) 때리맥였습니다.", "이(가) 찰과상을 입었습니다.", "이(가) 타박상을 입었습니다.", "을(를) 부엉이바위로 보냈습니다.", "을(를) 국정원 지하에 가뒀습니다."];
