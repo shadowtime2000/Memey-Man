6dc536acc1a29ff35c55a529bd8e1e901a55f7a5
calvinjung <64125023+calvinjung@users.noreply.github.com>
2020-05-29 09:38:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -109,9 +109,14 @@ bot.on('message', msg=>{
     }
 	
     if(msg.content.startsWith('?괴롭히기')) {
+	var member= msg.mentions.members.first();
 	var facts = ["을(를) 때리맥였습니다.", "이(가) 찰과상을 입었습니다.", "이(가) 타박상을 입었습니다.", "을(를) 부엉이바위로 보냈습니다.", "을(를) 국정원 지하에 가뒀습니다."];
 	var fact = Math.floor(Math.random() * facts.length);
-	msg.channel.send( member.displayName + facts[fact] )
+	const hitee = new Discord.MessageEmbed()
+		.setColor('#0099ff')
+		.setTitle('괴롭히기')
+		.setDescription( member.displayName + facts[fact] )
+	msg.channel.send(hitee)
     }
 	
     if(msg.content == "?뜻"){
