448dc83c058d9037feb5e5a8f03795fb921f863e
calvinjung <myadiuio@gmail.com>
2020-06-01 13:45:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -63,11 +63,7 @@ bot.on('message', msg=>{
     
     }
 
-	
     if(msg.content.startsWith('?harass')) {
-        const args = msg.content.split(' ').slice(1); 
-        const memberping = args.join(' '); 
-        if (!memberping) return msg.reply('Ping a member after ?harass')
         var facts = [" is so stupid.", " is ugly", " is an idiot.", " should disappear.", " looks like Kim Jong Un."];
         var fact = Math.floor(Math.random() * facts.length);
         const hitee = new Discord.MessageEmbed()
@@ -75,7 +71,10 @@ bot.on('message', msg=>{
             .setTitle('You harassed ' + member.displayName  + '!' )
             .setDescription( member.displayName + facts[fact] )
             .setFooter('Ping a member to use ?harass command!')
-        msg.channel.send(hitee)
+        const args = msg.content.split(' ').slice(1); 
+        const memberping = args.join(' '); 
+        if (!memberping) return msg.reply('Ping a member after ?harass')
+            msg.channel.send(hitee)
     }
 	
     if(msg.content == "?help"){
