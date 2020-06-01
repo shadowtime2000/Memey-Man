c4075a989cc1e3017e5206af7611b5d68d27cb50
calvinjung <myadiuio@gmail.com>
2020-06-01 09:39:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
-const axios = require("axios");
-const cheerio = require("cheerio");
-const log = console.log
 const bot = new Discord.Client();
 
 const token = 'NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU';
@@ -40,43 +37,44 @@ bot.on('message', msg=>{
 	
     if(msg.content.startsWith('?purge')) {	
 
-	const nopurge = new Discord.MessageEmbed()
-		.setColor('#FF0000')
-		.setTitle('Oops!')
-		.setDescription("You can't use that command!")
+        const nopurge = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Oops!')
+            .setDescription("You can't use that command!")
 
-	if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(nopurge);
-	const args = msg.content.split(' ').slice(1); 
-	const amount = args.join(' '); 
-	const extranum = "1"
-	const amountaa = parseInt(amount)
-	const extranumaa = parseInt(extranum)
-	const messageamount = amountaa + extranumaa
+        if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(nopurge);
+        const args = msg.content.split(' ').slice(1); 
+        const amount = args.join(' '); 
+        const extranum = "1"
+        const amountaa = parseInt(amount)
+        const extranumaa = parseInt(extranum)
+        const messageamount = amountaa + extranumaa
 
-	if (!amount) return msg.reply('Give me how many messages should I purge!'); 
-	if (isNaN(amount)) return msg.reply('Give me a number!'); 
+        if (!amount) return msg.reply('Give me how many messages should I purge!'); 
+        if (isNaN(amount)) return msg.reply('Give me a number!'); 
 
-	if (amount > 99) return msg.reply("Too many messages to purge! Give me a smaller number."); 
-	if (amount < 1) return msg.reply("You can't purge less than 1 message! Give me a bigger number."); 
-	
-	msg.channel.messages.fetch({ limit: messageamount }).then(messages => {
-	    msg.channel.bulkDelete(messages 
+        if (amount > 99) return msg.reply("Too many messages to purge! Give me a smaller number."); 
+        if (amount < 1) return msg.reply("You can't purge less than 1 message! Give me a bigger number."); 
+        
+        msg.channel.messages.fetch({ limit: messageamount }).then(messages => {
+            msg.channel.bulkDelete(messages 
+
 	)});	
     }
 	
     if(msg.content.startsWith('?harass')) {
-	const args = msg.content.split(' ').slice(1); 
-	const memberping = args.join(' '); 
-        if (!memberping) return msg.reply('Ping a member after ?harass')
-	var member= msg.mentions.members.first();
-	var facts = [" is so stupid.", " is ugly", " is an idiot.", " should disappear.", " looks like Kim Jong Un."];
-	var fact = Math.floor(Math.random() * facts.length);
-	const hitee = new Discord.MessageEmbed()
-		.setColor('#ffa500')
-		.setTitle('You harassed ' + member.displayName  + '!' )
-		.setDescription( member.displayName + facts[fact] )
-		.setFooter('Ping a member to use ?harass command!')
-	msg.channel.send(hitee)
+        const args = msg.content.split(' ').slice(1); 
+        const memberping = args.join(' '); 
+            if (!memberping) return msg.reply('Ping a member after ?harass')
+        var member= msg.mentions.members.first();
+        var facts = [" is so stupid.", " is ugly", " is an idiot.", " should disappear.", " looks like Kim Jong Un."];
+        var fact = Math.floor(Math.random() * facts.length);
+        const hitee = new Discord.MessageEmbed()
+            .setColor('#ffa500')
+            .setTitle('You harassed ' + member.displayName  + '!' )
+            .setDescription( member.displayName + facts[fact] )
+            .setFooter('Ping a member to use ?harass command!')
+        msg.channel.send(hitee)
     }
 	
     if(msg.content == "?help"){
@@ -99,44 +97,44 @@ bot.on('message', msg=>{
     }
 
     if(msg.content.startsWith('?kick')) {
-	const args = msg.content.split(' ').slice(2); 
-	const kickreason = args.join(' '); 
-	const noperm = new Discord.MessageEmbed()
-		.setColor('#FF0000')
-		.setTitle('Oops!')
-		.setDescription("You can't use that command!")
-	if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(noperm);
-        if(!kickreason) return msg.reply("Give me a reason!")
-        var member= msg.mentions.members.first();
-        member.kick().then((member) => {
-            const kickembed = new Discord.MessageEmbed()
-                .setColor('#ffff00')
-                .setTitle('**Sucessfully kicked member**')
-                .setDescription( 'Kicked ' + member.displayName + '.' + '\nReason: ' + kickreason );
-            msg.channel.send(kickembed)
-        }).catch(() => {
-            msg.channel.send("Can't kick that member!");
+        const args = msg.content.split(' ').slice(2); 
+        const kickreason = args.join(' '); 
+        const noperm = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Oops!')
+            .setDescription("You can't use that command!")
+        if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(noperm);
+            if(!kickreason) return msg.reply("Give me a reason!")
+            var member= msg.mentions.members.first();
+            member.kick().then((member) => {
+                const kickembed = new Discord.MessageEmbed()
+                    .setColor('#ffff00')
+                    .setTitle('**Sucessfully kicked member**')
+                    .setDescription( 'Kicked ' + member.displayName + '.' + '\nReason: ' + kickreason );
+                msg.channel.send(kickembed)
+            }).catch(() => {
+                msg.channel.send("Can't kick that member!");
         });
     }
 
     if(msg.content.startsWith('?ban')) {
-	const args = msg.content.split(' ').slice(2); 
-	const banreason = args.join(' '); 
-	const noperm1 = new Discord.MessageEmbed()
-		.setColor('#FF0000')
-		.setTitle('Oops!')
-		.setDescription("You can't use that command!")
-        if(!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send(noperm1);
-        if(!banreason) return msg.reply("Give me a reason!")
-        var member= msg.mentions.members.first();
-        member.ban().then((member) => {
-            const banembed = new Discord.MessageEmbed()
-                .setColor('#ffff00')
-                .setTitle('**Successfully banned member**')
-                .setDescription( 'Banned ' + member.displayName + '.' + '\nReason: ' + banreason );
-            msg.channel.send(banembed)
-        }).catch(() => {
-            msg.channel.send("Can't ban that member!");
+        const args = msg.content.split(' ').slice(2); 
+        const banreason = args.join(' '); 
+        const noperm1 = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Oops!')
+            .setDescription("You can't use that command!")
+            if(!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send(noperm1);
+            if(!banreason) return msg.reply("Give me a reason!")
+            var member= msg.mentions.members.first();
+            member.ban().then((member) => {
+                const banembed = new Discord.MessageEmbed()
+                    .setColor('#ffff00')
+                    .setTitle('**Successfully banned member**')
+                    .setDescription( 'Banned ' + member.displayName + '.' + '\nReason: ' + banreason );
+                msg.channel.send(banembed)
+            }).catch(() => {
+                msg.channel.send("Can't ban that member!");
         });
     }
        
