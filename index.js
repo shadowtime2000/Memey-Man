bfc73f57f588d88c3a020ca1e96c7a4523d2f468
calvinjung <myadiuio@gmail.com>
2020-06-02 09:13:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ bot.on('message', msg=>{
  
     if(msg.content == "?topic") {
         var member= msg.mentions.members.first();
-        var facts = ["What is your favourite drink?", "What is your favourite song?", "What did you do yesterday?", "What will you do tomorrow?", "What is your favourite movie?", "What is your favorite food?", "What games do you play?"];
+        var facts = ["What is your favorite drink?", "What is your favorite song?", "What did you do yesterday?", "What will you do tomorrow?", "What is your favorite movie?", "What is your favorite food?", "What games do you play?"];
         var fact = Math.floor(Math.random() * facts.length);
         const topicEmbed = new Discord.MessageEmbed()
                 .setColor('#7cfc00')
