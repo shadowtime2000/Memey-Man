25ddbc0508996d1269986d125e41efc453f65883
calvinjung <myadiuio@gmail.com>
2020-06-03 14:34:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -231,58 +231,7 @@ bot.on('message', msg=>{
         });
     }
        
-    var parts = message.content.split(" "); // Splits message into an array for every space, our layout: "<command> [search query]" will become ["<command>", "search query"]
- 
-    /* Simple command manager */
-    if (parts[0] === "!image") { // Check if first part of message is image command
- 
-        // call the image function
-        image(message, parts); // Pass requester message to image function
- 
-    }
- 
-});
- 
-function image(message, parts) {
- 
-    /* extract search query from message */
- 
-    var search = parts.slice(1).join(" "); // Slices of the command part of the array ["!image", "cute", "dog"] ---> ["cute", "dog"] ---> "cute dog"
- 
-    var options = {
-        url: "http://results.dogpile.com/serp?qc=images&q=" + search,
-        method: "GET",
-        headers: {
-            "Accept": "text/html",
-            "User-Agent": "Chrome"
-        }
-    };
-    request(options, function(error, response, responseBody) {
-        if (error) {
-            // handle error
-            return;
-        }
- 
-        /* Extract image URLs from responseBody using cheerio */
- 
-        $ = cheerio.load(responseBody); // load responseBody into cheerio (jQuery)
- 
-        // In this search engine they use ".image a.link" as their css selector for image links
-        var links = $(".image a.link");
- 
-        // We want to fetch the URLs not the DOM nodes, we do this with jQuery's .attr() function
-        // this line might be hard to understand but it goes thru all the links (DOM) and stores each url in an array called urls
-        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-        console.log(urls);
-        if (!urls.length) {
-            // Handle no results
-            return;
-        }
- 
-        // Send result
-        message.channel.send( urls[0] );
-    });
- 
-}
+    
+})
 
 bot.login(process.env.token);
