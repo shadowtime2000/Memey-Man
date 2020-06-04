7902011492918f1c24e1e2d16260d8bfff24b125
calvinjung <myadiuio@gmail.com>
2020-06-04 11:40:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -68,10 +68,10 @@ bot.on('message', msg=>{
         const args = msg.content.split(' ').slice(1);
         const repeatword = args.join(' ')
         if(!repeatword) return msg.reply('Nothing to repeat!');
+        if(repeatword.includes("?repeat")) return msg.reply("You can't repeat more than once.")
         if(repeatword.includes("?kick")) return msg.reply("Don't try it!")
         if(repeatword.includes("?ban")) return msg.reply("Don't try it!")
-        if(repeatword.includes("?purge")) return msg.reply("Don't try it!")
-        if(repeatword.includes("?repeat")) return msg.reply("You can't repeat more than once.")
+        if(repeatword.includes("?purge")) return msg.reply("Don't try it!")      
         msg.channel.send(repeatword)
 	}
 
