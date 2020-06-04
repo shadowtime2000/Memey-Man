e459223af48d550d79c3f2de827119ba7ed30a6b
calvinjung <myadiuio@gmail.com>
2020-06-04 09:57:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -116,18 +116,16 @@ bot.on('message', msg=>{
 
     if(msg.content.startsWith('?punch')) {
         var member= msg.mentions.members.first();
-        const args = msg.content.split(' ').slice(2); 
-        const times = args.join(' '); 
         const spanke = new Discord.MessageEmbed
             .setColor('#ffa500')
             .setTitle('?punch command')
             .setDescription('Usage: ?punch [member ping]')
         if (!member) return msg.channel.send(spanke)
-            const hitee = new Discord.MessageEmbed()
-                .setColor('#ffa500')
-                .setTitle( msg.member.displayName + " punched!" )
-                .setDescription( msg.member.displayName + ' punched ' + member.displayName + "!" )
-                .setFooter( "Command usage: ?punch [member ping]" )
+        const hitee = new Discord.MessageEmbed()
+            .setColor('#ffa500')
+            .setTitle( msg.member.displayName + " punched!" )
+            .setDescription( msg.member.displayName + ' punched ' + member.displayName + "!" )
+            .setFooter( "Command usage: ?punch [member ping]" )
         msg.channel.send(hitee)
     }
 	
