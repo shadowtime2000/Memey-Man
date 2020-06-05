4b3e54e7601409115ff2da70b4af4552665972f6
calvinjung <myadiuio@gmail.com>
2020-06-05 16:00:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ bot.on("message", async msg => {
             });
     }
 
+    if(msg.content == prefix + "xue") {
+        msg.channel.send("𝔁𝓾𝓮:cold_face:𝓱𝓾𝓪:woman_fairy:𝓹𝓲𝓪𝓸:heart_eyes_cat:𝓹𝓲𝓪𝓸:moyai:𝓫𝓮𝓲:japanese_goblin:𝓯𝓮𝓷𝓰:star_struck:𝔁𝓲𝓪𝓸:smirk_cat:𝔁𝓲𝓪𝓸:footprints:")
+    }
+    
     if(msg.content == prefix + "coinflip") {
         var facts = ["Head", "tail"]
         var fact = Math.floor(Math.random() * facts.length);
