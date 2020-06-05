1467aeff7ead8ee72e268c0146753b1cc27b25ed
calvinjung <myadiuio@gmail.com>
2020-06-05 14:24:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,24 +13,6 @@ bot.on("ready", () =>{
 var prefix = "?"
 
 bot.on('message', msg=>{
-
-    bot.on("guildMemberAdd", (member) => {
-        const guild = member.guild;
-        if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
-        newUsers[guild.id].set(member.id, member.user);
-      
-        if (newUsers[guild.id].size > 10) {
-          const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-          guild.channels.find(channel => channel.name == "general").send("Welcome our new users!\n" + userlist);
-          newUsers[guild.id].clear();
-        }
-      });
-      
-      bot.on("guildMemberRemove", (member) => {
-        const guild = member.guild;
-        if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
-
-    })
     
     if(msg.content == prefix + "meme") {
         fetch('https://meme-api.herokuapp.com/gimme')
