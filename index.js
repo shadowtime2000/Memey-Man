25c23bf5806f2d3bdc453dbe3cf647bdb1f7723d
calvinjung <myadiuio@gmail.com>
2020-06-15 00:02:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ bot.on("message", async msg => {
         if(repeatword.includes(prefix + "kick")) return msg.reply("Don't try it!")
         if(repeatword.includes(prefix + "ban")) return msg.reply("Don't try it!")
         if(repeatword.includes(prefix + "purge")) return msg.reply("Don't try it!")   
-        if(repeatword.includes("everyone")) return msg.reply("Don't try it!")    
+        if(repeatword.includes("@everyone")) return msg.reply("Don't try it!")    
         msg.channel.send(repeatword)
 	}
 
