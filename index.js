52149b6c9af1282dcdafb8277bc56d8894944bfc
calvinjung <myadiuio@gmail.com>
2020-06-15 10:25:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ bot.on("ready", () =>{
     setInterval(() => {
         const index = Math.floor(Math.random() * activities_list.length); 
         bot.user.setActivity(activities_list[index], {type: "WATCHING"}); 
-    }, 3000); 
+    }, 7000); 
 });
 
 var prefix = "&"
