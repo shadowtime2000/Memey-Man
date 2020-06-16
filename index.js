6aaa143e89013d10b5aee72443c268a5ab11d1fe
calvinjung <myadiuio@gmail.com>
2020-06-16 16:32:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -360,10 +360,16 @@ bot.on("message", async msg => {
             .setTitle('Oops!')
             .setDescription("You can't use that command!")
 
+        const nonum = new Discord.MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle('Oops!')
+            .setDescription("Number should be less than or equal to 21600.")
+
         if(!msg.member.hasPermission("MANAGE_CHANNELS")) return msg.channel.send(noperm1)
 
         var str = msg.content.split(prefix + 'slowmode ');
         var num = parseInt(str[1], 10);
+        if(num > 21600) return msg.channel.send(nonum)
         msg.channel.setRateLimitPerUser(num).then(() => {
             const slowmode = new Discord.MessageEmbed()
                 .setColor('#EE0000')
