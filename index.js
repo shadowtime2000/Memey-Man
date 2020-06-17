3d59ed8d1c99a44adee905c2e3f97fdc03d4274e
calvinjung <myadiuio@gmail.com>
2020-06-17 11:20:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ bot.on("message", async msg => {
                 .setImage(bot.users.cache.get(user.id).displayAvatarURL());
             msg.channel.send(useravatarEmbed);
         } else {
-            const iduser = bot.fetchUser(memberid);
+            const iduser = bot.users.fetch(memberid);
             const idavatarEmbed = new Discord.MessageEmbed()
                 .setColor(`#006a4e`)
                 .setTitle( iduser.displayName + `'s avatar`)
