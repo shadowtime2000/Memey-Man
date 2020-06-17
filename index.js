3359f33c6618d2cfeddd7285f9e514d9068ca9a0
calvinjung <myadiuio@gmail.com>
2020-06-17 10:57:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -208,8 +208,22 @@ bot.on("message", async msg => {
         const hitee = new Discord.MessageEmbed()
             .setColor('#ffa500')
             .setTitle( msg.member.displayName + " :right_facing_fist: " + member.displayName)
-            .setDescription( msg.author.toString() + ' punched ' + member.toString() )
+            .setDescription( msg.author.toString() + ' punched ' + member.toString() + "!" )
         msg.channel.send(hitee)
+    }
+
+    if(msg.content.startsWith(prefix + 'hug')) {
+        var member= msg.mentions.members.first();
+        const huge = new Discord.MessageEmbed()
+            .setColor('#ffa500')
+            .setTitle('Hug command')
+            .setDescription('Usage: &hug [member mention]')
+        if (!member) return msg.channel.send(huge)
+        const hugee = new Discord.MessageEmbed()
+            .setColor('#ffa500')
+            .setTitle( msg.member.displayName + " :hugged: " + member.displayName )
+            .setDescription( msg.author.toString() + ' hugged ' + member.toString() + "!" )
+        msg.channel.send(hugee)
     }
 
     if (msg.content.startsWith(prefix + 'embed')) {
@@ -228,7 +242,7 @@ bot.on("message", async msg => {
         const helpEmbed = new Discord.MessageEmbed()
 	    .setColor('#0099ff')
             .setTitle('**COMMAND LIST**')
-            .setDescription(`**Information commands**\n&botinfo : Gives bot info.\n&help : This command.\n&invite: Gives an invite to Memey Man server.\n\n**Main commands**\n&ping : Gives current ping.\n&meme : Gives a random meme.\n&urban : Gives urban dictionary definition.\n&punch : Punches a user!\n&avatar: Gives your/someone's avatar.\n&8ball : Answers your question.\n&topic : Gives a chat topic.\n\n**Text commands**\n&repeat : Repeats your message.\n&embed : Puts your message in an embed.\n&hm: HMs your message.\n&xue : Xue hua piao piao bei feng...\n\n**Utility commands**\n&coinflip : Flips a coin.\n&diceroll : Rolls a dice.\n\n**Moderator commands**\n&ban : Bans a user.\n&kick : Kicks a user.\n&purge : Deletes messages.\n&slowmode: Sets slowmode.`)
+            .setDescription(`**Information commands**\n&botinfo : Gives bot info.\n&help : This command.\n&invite: Gives an invite to Memey Man server.\n\n**Main commands**\n&ping : Gives current ping.\n&meme : Gives a random meme.\n&urban : Gives urban dictionary definition.\n&punch : Punches a user!\n&hug: Hugs a user!\n&avatar: Gives your/someone's avatar.\n&8ball : Answers your question.\n&topic : Gives a chat topic.\n\n**Text commands**\n&repeat : Repeats your message.\n&embed : Puts your message in an embed.\n&hm: HMs your message.\n&xue : Xue hua piao piao bei feng...\n\n**Utility commands**\n&vote: Creates a vote.\n&coinflip : Flips a coin.\n&diceroll : Rolls a dice.\n\n**Moderator commands**\n&ban : Bans a user.\n&kick : Kicks a user.\n&purge : Deletes messages.\n&slowmode: Sets slowmode.`)
 	    msg.channel.send(helpEmbed); 
 	    
     }
@@ -391,7 +405,6 @@ bot.on("message", async msg => {
             });
         }
     }
-
     
     if(msg.content.startsWith(prefix + 'urban')) {
         const args = msg.content.split(' ').slice(1); 
