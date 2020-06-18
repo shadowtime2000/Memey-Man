5a98cc69f5b8345c13fcb86bad42ac1f5e7a8764
calvinjung <myadiuio@gmail.com>
2020-06-18 12:06:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -132,6 +132,19 @@ bot.on("message", async msg => {
             });
     }
 
+    if(msg.content == prefix + "elephant") {
+        fetch('https://elephant-api.herokuapp.com/elephants/random')
+            .then(res => res.json())
+            .then(json => {
+                const eleohantembed = new Discord.MessageEmbed()
+                    .setColor('#8B4513')
+                    .setTitle(":elephant: Elephant image!")
+                    .setImage(json.image)
+                    //.setTimestamp()
+                msg.channel.send(elephantembed)
+            });
+    }
+
     if(msg.content == prefix + "dog") {
         fetch('https://dog.ceo/api/breeds/image/random')
             .then(res => res.json())
