8cfbce72304a717622ae8f2ea7b13fd901576343
calvinjung <myadiuio@gmail.com>
2020-06-19 16:42:52 +0900

    1

--- index.js
+++ index.js
@@ -110,22 +110,22 @@ bot.on("message", async msg => {
         if(!memberid && !user) {
             const avatarEmbed = new Discord.MessageEmbed()
                 .setColor(`#006a4e`)
-                .setAuthor(bot.users.cache.get(msg.author.id).tag, bot.users.cache.get(msg.author.id).displayAvatarURL())
-                .setImage(msg.author.displayAvatarURL());
+                .setAuthor(bot.users.cache.get(msg.author.id).tag, bot.users.cache.get(msg.author.id).displayAvatarURL({ format: 'png' }))
+                .setImage(msg.author.displayAvatarURL({ format: 'png' }));
             msg.channel.send(avatarEmbed);
         }
         if(user) {
             const useravatarEmbed = new Discord.MessageEmbed()            
                 .setColor(`#006a4e`)
-                .setAuthor(bot.users.cache.get(user.id).tag, bot.users.cache.get(user.id).displayAvatarURL())
-                .setImage(bot.users.cache.get(user.id).displayAvatarURL());
+                .setAuthor(bot.users.cache.get(user.id).tag, bot.users.cache.get(user.id).displayAvatarURL({ format: 'png' }))
+                .setImage(bot.users.cache.get(user.id).displayAvatarURL({ format: 'png' }));
             msg.channel.send(useravatarEmbed);
         }
         if(memberid && !user && msg.guild.member(memberid)) {
             const idavatarEmbed = new Discord.MessageEmbed() 
                 .setColor(`#006a4e`)
-                .setAuthor(bot.users.cache.get(memberid).tag, bot.users.cache.get(memberid).displayAvatarURL())
-                .setImage(bot.users.cache.get(memberid).displayAvatarURL());
+                .setAuthor(bot.users.cache.get(memberid).tag, bot.users.cache.get(memberid).displayAvatarURL({ format: 'png' }))
+                .setImage(bot.users.cache.get(memberid).displayAvatarURL({ format: 'png' }));
             msg.channel.send(idavatarEmbed);
         } 
         if(memberid && !user && !msg.guild.member(memberid) ) return msg.channel.send(":x: No results found.")
