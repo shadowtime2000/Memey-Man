b6caa339fd907b671918964e584b1ef23471edde
calvinjung <myadiuio@gmail.com>
2020-06-19 18:08:37 +0900

    1

---
+++ commands/serverinfo.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'serverinfo',
+	description: 'serverinfo command',
+	execute(msg, args) {
+        const serverembed = new Discord.MessageEmbed()
+            .setTitle("Server info")
+            .setDescription(`Server name: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`)
+            .setTimestamp()
+        msg.channel.send(serverembed)
+	},
+};
--- index.js
+++ index.js
@@ -63,6 +63,9 @@ bot.on("message", async msg => {
     if(command == "help") {
         bot.commands.get('help').execute(msg, args);
     }
+    if(command == "serverinfo") {
+        bot.commands.get('serverinfo').execute(msg, args);
+    }
     if(command.startsWith("8ball")) {
         bot.commands.get('8ball').execute(msg, args);
     }
@@ -175,59 +178,59 @@ bot.on("message", async msg => {
         });
     }
 
-    // if(msg.content.startsWith(prefix + 'kick')) {
+    if(msg.content.startsWith(prefix + 'kick')) {
 
-    //     if (msg.channel.type == "dm") return;
+        if (msg.channel.type == "dm") return;
 
-    //     const args = msg.content.split(' ').slice(2); 
-    //     const kickreason = args.join(' '); 
-    //     var mem= msg.mentions.members.first();
+        const args = msg.content.split(' ').slice(2); 
+        const kickreason = args.join(' '); 
+        var mem= msg.mentions.members.first();
 
-    //     const noperm = new Discord.MessageEmbed()
-    //         .setColor('#FFFF00')
-    //         .setTitle('Oops!')
-    //         .setDescription("You can't use that command!")
+        const noperm = new Discord.MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle('Oops!')
+            .setDescription("You can't use that command!")
         
-    //     const nomemmber = new Discord.MessageEmbed()
-    //         .setColor('#FFFF00')
-    //         .setTitle('Oops!')
-    //         .setDescription("Give a member to kick!")
+        const nomemmber = new Discord.MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle('Oops!')
+            .setDescription("Give a member to kick!")
 
-    //     const noreason = new Discord.MessageEmbed()
-    //         .setColor('#FFFF00')
-    //         .setTitle('Oops!')
-    //         .setDescription("Give me a reason!")
+        const noreason = new Discord.MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle('Oops!')
+            .setDescription("Give me a reason!")
 
-    //     const cantkick = new Discord.MessageEmbed()
-    //         .setColor('#FFFF00')
-    //         .setTitle('Oops!')
-    //         .setDescription("An errror occurred.")
+        const cantkick = new Discord.MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle('Oops!')
+            .setDescription("An errror occurred.")
 
-    //         if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(noperm);
-    //         if(!mem) return msg.channel.send(nomemmber)
-    //         if(!kickreason) return msg.channel.send(noreason)
+            if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(noperm);
+            if(!mem) return msg.channel.send(nomemmber)
+            if(!kickreason) return msg.channel.send(noreason)
             
-    //         const kickembed = new Discord.MessageEmbed()
-    //             .setColor('#ff0000')
-    //             .setTitle('**Successfully kicked member**')
-    //             .setDescription( `**Kicked ${mem.displayName}.**` + '\n**Moderator**: ' + msg.member.displayName + '\n**Reason**: ' + kickreason )
-    //             .setTimestamp()
+            const kickembed = new Discord.MessageEmbed()
+                .setColor('#ff0000')
+                .setTitle('**Successfully kicked member**')
+                .setDescription( `**Kicked ${mem.displayName}.**` + '\n**Moderator**: ' + msg.member.displayName + '\n**Reason**: ' + kickreason )
+                .setTimestamp()
 
-    //         const dmkickembed = new Discord.MessageEmbed()
-    //             .setColor('#ff0000')
-    //             .setTitle(`**You have been kicked from ${msg.guild.name}**`)
-    //             .setDescription( '**Moderator**: ' + msg.member.displayName + '\n**Reason**: ' + kickreason )
-    //             .setTimestamp()
+            const dmkickembed = new Discord.MessageEmbed()
+                .setColor('#ff0000')
+                .setTitle(`**You have been kicked from ${msg.guild.name}**`)
+                .setDescription( '**Moderator**: ' + msg.member.displayName + '\n**Reason**: ' + kickreason )
+                .setTimestamp()
 
-    //         mem.send(dmkickembed)
-    //         .then(() => {
-    //             mem.kick().then((member) => {
-    //             msg.channel.send(kickembed)
-    //             })
-    //         }).catch(() => {
-    //             msg.channel.send(cantkick);
-    //     });
-    // }
+            mem.send(dmkickembed)
+            .then(() => {
+                mem.kick().then((member) => {
+                msg.channel.send(kickembed)
+                })
+            }).catch(() => {
+                msg.channel.send(cantkick);
+        });
+    }
 
     if(msg.content.startsWith(prefix + 'ban')) {
 
