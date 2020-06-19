f62f29cdd3f8253e2e164bd08aa82ab0c6499582
calvinjung <myadiuio@gmail.com>
2020-06-19 09:58:33 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,9 +2,7 @@ module.exports = {
 	name: 'ping',
 	description: 'ping command',
 	execute(msg, args) {
-        async function declaredAsAsync() {
-            const m = msg.channel.send("Pong:");
-            m.edit(`Pong: ${m.createdTimestamp - msg.createdTimestamp}ms`);
-        }
+        const m = msg.channel.send("Pong:");
+        m.edit(`Pong: ${m.createdTimestamp - msg.createdTimestamp}ms`);
 	},
 };
