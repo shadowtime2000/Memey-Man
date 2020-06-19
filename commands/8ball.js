dd529897fbd130bab6399f2b615cc5157ab5275c
calvinjung <myadiuio@gmail.com>
2020-06-19 11:47:01 +0900

    1

---
+++ commands/8ball.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'coinflip',
+	description: 'coinflip command',
+	execute(msg, args) {
+        const args1 = msg.content.split(' ').slice(1); 
+        const question = args1.join(' '); 
+        var facts = ["Yes.", "No.", "I don't know", "Of course.", "Never.", "Maybe.", "Hmm...", "Excuse me?"];
+        var fact = Math.floor(Math.random() * facts.length);
+        const ballembed = new Discord.MessageEmbed()
+            .setColor('#9c51b6')
+            .setTitle('**8ball**')
+            .setDescription( '**Your question**: ' + question + '\n**My answer**: ' + facts[fact] )
+            .setTimestamp()
+        msg.channel.send(ballembed);
+	},
+};
--- index.js
+++ index.js
@@ -148,18 +148,18 @@ bot.on("message", async msg => {
         msg.channel.send(repeatword)
 	}
 
-    if(msg.content.startsWith(prefix + '8ball')) {
-        const args = msg.content.split(' ').slice(1); 
-        const question = args.join(' '); 
-        var facts = ["Yes.", "No.", "I don't know", "Of course.", "Never.", "Maybe.", "Hmm...", "Excuse me?"];
-        var fact = Math.floor(Math.random() * facts.length);
-        const ballembed = new Discord.MessageEmbed()
-            .setColor('#9c51b6')
-            .setTitle('**8ball**')
-            .setDescription( '**Your question**: ' + question + '\n**My answer**: ' + facts[fact] )
-            .setTimestamp()
-        msg.channel.send(ballembed);
-    }
+    // if(msg.content.startsWith(prefix + '8ball')) {
+    //     const args = msg.content.split(' ').slice(1); 
+    //     const question = args.join(' '); 
+    //     var facts = ["Yes.", "No.", "I don't know", "Of course.", "Never.", "Maybe.", "Hmm...", "Excuse me?"];
+    //     var fact = Math.floor(Math.random() * facts.length);
+    //     const ballembed = new Discord.MessageEmbed()
+    //         .setColor('#9c51b6')
+    //         .setTitle('**8ball**')
+    //         .setDescription( '**Your question**: ' + question + '\n**My answer**: ' + facts[fact] )
+    //         .setTimestamp()
+    //     msg.channel.send(ballembed);
+    // }
 	
     if(msg.content.startsWith(prefix + 'purge')) {	
 
