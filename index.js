61a345013d01b18d72a448153bc9f1c3454079ac
calvinjung <myadiuio@gmail.com>
2020-06-19 17:13:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -168,21 +168,7 @@ bot.on("message", async msg => {
             msg.react('✅')
         });
     }
-
-    // if(msg.content.startsWith(prefix + 'punch')) {
-    //     var member= msg.mentions.members.first();
-    //     const spanke = new Discord.MessageEmbed()
-    //         .setColor('#ffa500')
-    //         .setTitle('Punch command')
-    //         .setDescription('Usage: &punch [member mention]')
-    //     if (!member) return msg.channel.send(spanke)
-    //     const hitee = new Discord.MessageEmbed()
-    //         .setColor('#ffa500')
-    //         .setTitle( msg.member.displayName + " :right_facing_fist: " + member.displayName)
-    //         .setDescription( msg.author.toString() + ' punched ' + member.toString() + "!" )
-    //     msg.channel.send(hitee)
-    // }
-
+    
     if(msg.content.startsWith(prefix + 'hug')) {
         var member= msg.mentions.members.first();
         const huge = new Discord.MessageEmbed()
