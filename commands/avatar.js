655723417f0ce634908d470c92f2af0f823f31b7
calvinjung <myadiuio@gmail.com>
2020-06-19 17:22:30 +0900

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -10,21 +10,21 @@ module.exports = {
         if(!memberid && !user) {
             const avatarEmbed = new Discord.MessageEmbed()
                 .setColor(`#006a4e`)
-                .setAuthor(bot.users.cache.get(msg.author.id).tag, bot.users.cache.get(msg.author.id).displayAvatarURL({ format: 'png' }))
+                .setAuthor(bot.users.cache.get(msg.author.id).displayName, bot.users.cache.get(msg.author.id).displayAvatarURL({ format: 'png' }))
                 .setImage(msg.author.displayAvatarURL({ format: 'png' }));
             msg.channel.send(avatarEmbed);
         }
         if(user) {
             const useravatarEmbed = new Discord.MessageEmbed()            
                 .setColor(`#006a4e`)
-                .setAuthor(bot.users.cache.get(user.id).tag, bot.users.cache.get(user.id).displayAvatarURL({ format: 'png' }))
+                .setAuthor(bot.users.cache.get(user.id).displayName, bot.users.cache.get(user.id).displayAvatarURL({ format: 'png' }))
                 .setImage(bot.users.cache.get(user.id).displayAvatarURL({ format: 'png' }));
             msg.channel.send(useravatarEmbed);
         }
         if(memberid && !user && msg.guild.member(memberid)) {
             const idavatarEmbed = new Discord.MessageEmbed() 
                 .setColor(`#006a4e`)
-                .setAuthor(bot.users.cache.get(memberid).tag, bot.users.cache.get(memberid).displayAvatarURL({ format: 'png' }))
+                .setAuthor(bot.users.cache.get(memberid).displayName, bot.users.cache.get(memberid).displayAvatarURL({ format: 'png' }))
                 .setImage(bot.users.cache.get(memberid).displayAvatarURL({ format: 'png' }));
             msg.channel.send(idavatarEmbed);
         } 
