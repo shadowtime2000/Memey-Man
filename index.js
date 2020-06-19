d53009d6416d77eecdf7952c99ef7e485b3b4056
calvinjung <myadiuio@gmail.com>
2020-06-19 17:27:48 +0900

    1

--- index.js
+++ index.js
@@ -112,34 +112,34 @@ bot.on("message", async msg => {
     }
 
 
-    // if(msg.content.startsWith(prefix + 'avatar')) {
-    //     var user = msg.mentions.members.first()
-    //     const arguments = msg.content.split(' ').slice(1); 
-    //     const memberid = arguments.join(' '); 
-    //     if(!memberid && !user) {
-    //         const avatarEmbed = new Discord.MessageEmbed()
-    //             .setColor(`#006a4e`)
-    //             .setAuthor(bot.users.cache.get(msg.author.id).tag, bot.users.cache.get(msg.author.id).displayAvatarURL({ format: 'png' }))
-    //             .setImage(msg.author.displayAvatarURL({ format: 'png' }));
-    //         msg.channel.send(avatarEmbed);
-    //     }
-    //     if(user) {
-    //         const useravatarEmbed = new Discord.MessageEmbed()            
-    //             .setColor(`#006a4e`)
-    //             .setAuthor(bot.users.cache.get(user.id).tag, bot.users.cache.get(user.id).displayAvatarURL({ format: 'png' }))
-    //             .setImage(bot.users.cache.get(user.id).displayAvatarURL({ format: 'png' }));
-    //         msg.channel.send(useravatarEmbed);
-    //     }
-    //     if(memberid && !user && msg.guild.member(memberid)) {
-    //         const idavatarEmbed = new Discord.MessageEmbed() 
-    //             .setColor(`#006a4e`)
-    //             .setAuthor(bot.users.cache.get(memberid).tag, bot.users.cache.get(memberid).displayAvatarURL({ format: 'png' }))
-    //             .setImage(bot.users.cache.get(memberid).displayAvatarURL({ format: 'png' }));
-    //         msg.channel.send(idavatarEmbed);
-    //     } 
-    //     if(memberid && !user && !msg.guild.member(memberid) ) return msg.channel.send(":x: No results found.")
+    if(msg.content.startsWith(prefix + 'avatar')) {
+        var user = msg.mentions.members.first()
+        const arguments = msg.content.split(' ').slice(1); 
+        const memberid = arguments.join(' '); 
+        if(!memberid && !user) {
+            const avatarEmbed = new Discord.MessageEmbed()
+                .setColor(`#006a4e`)
+                .setAuthor(bot.users.cache.get(msg.author.id).tag, bot.users.cache.get(msg.author.id).displayAvatarURL({ format: 'png' }))
+                .setImage(msg.author.displayAvatarURL({ format: 'png' }));
+            msg.channel.send(avatarEmbed);
+        }
+        if(user) {
+            const useravatarEmbed = new Discord.MessageEmbed()            
+                .setColor(`#006a4e`)
+                .setAuthor(bot.users.cache.get(user.id).tag, bot.users.cache.get(user.id).displayAvatarURL({ format: 'png' }))
+                .setImage(bot.users.cache.get(user.id).displayAvatarURL({ format: 'png' }));
+            msg.channel.send(useravatarEmbed);
+        }
+        if(memberid && !user && msg.guild.member(memberid)) {
+            const idavatarEmbed = new Discord.MessageEmbed() 
+                .setColor(`#006a4e`)
+                .setAuthor(bot.users.cache.get(memberid).tag, bot.users.cache.get(memberid).displayAvatarURL({ format: 'png' }))
+                .setImage(bot.users.cache.get(memberid).displayAvatarURL({ format: 'png' }));
+            msg.channel.send(idavatarEmbed);
+        } 
+        if(memberid && !user && !msg.guild.member(memberid) ) return msg.channel.send(":x: No results found.")
 
-    // }
+    }
 
     if(msg.content.startsWith(prefix + "vote")) {
 
@@ -175,59 +175,59 @@ bot.on("message", async msg => {
         });
     }
 
-    if(msg.content.startsWith(prefix + 'kick')) {
+    // if(msg.content.startsWith(prefix + 'kick')) {
 
-        if (msg.channel.type == "dm") return;
+    //     if (msg.channel.type == "dm") return;
 
-        const args = msg.content.split(' ').slice(2); 
-        const kickreason = args.join(' '); 
-        var mem= msg.mentions.members.first();
+    //     const args = msg.content.split(' ').slice(2); 
+    //     const kickreason = args.join(' '); 
+    //     var mem= msg.mentions.members.first();
 
-        const noperm = new Discord.MessageEmbed()
-            .setColor('#FFFF00')
-            .setTitle('Oops!')
-            .setDescription("You can't use that command!")
+    //     const noperm = new Discord.MessageEmbed()
+    //         .setColor('#FFFF00')
+    //         .setTitle('Oops!')
+    //         .setDescription("You can't use that command!")
         
-        const nomemmber = new Discord.MessageEmbed()
-            .setColor('#FFFF00')
-            .setTitle('Oops!')
-            .setDescription("Give a member to kick!")
+    //     const nomemmber = new Discord.MessageEmbed()
+    //         .setColor('#FFFF00')
+    //         .setTitle('Oops!')
+    //         .setDescription("Give a member to kick!")
 
-        const noreason = new Discord.MessageEmbed()
-            .setColor('#FFFF00')
-            .setTitle('Oops!')
-            .setDescription("Give me a reason!")
+    //     const noreason = new Discord.MessageEmbed()
+    //         .setColor('#FFFF00')
+    //         .setTitle('Oops!')
+    //         .setDescription("Give me a reason!")
 
-        const cantkick = new Discord.MessageEmbed()
-            .setColor('#FFFF00')
-            .setTitle('Oops!')
-            .setDescription("An errror occurred.")
+    //     const cantkick = new Discord.MessageEmbed()
+    //         .setColor('#FFFF00')
+    //         .setTitle('Oops!')
+    //         .setDescription("An errror occurred.")
 
-            if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(noperm);
-            if(!mem) return msg.channel.send(nomemmber)
-            if(!kickreason) return msg.channel.send(noreason)
+    //         if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(noperm);
+    //         if(!mem) return msg.channel.send(nomemmber)
+    //         if(!kickreason) return msg.channel.send(noreason)
             
-            const kickembed = new Discord.MessageEmbed()
-                .setColor('#ff0000')
-                .setTitle('**Successfully kicked member**')
-                .setDescription( `**Kicked ${mem.displayName}.**` + '\n**Moderator**: ' + msg.member.displayName + '\n**Reason**: ' + kickreason )
-                .setTimestamp()
+    //         const kickembed = new Discord.MessageEmbed()
+    //             .setColor('#ff0000')
+    //             .setTitle('**Successfully kicked member**')
+    //             .setDescription( `**Kicked ${mem.displayName}.**` + '\n**Moderator**: ' + msg.member.displayName + '\n**Reason**: ' + kickreason )
+    //             .setTimestamp()
 
-            const dmkickembed = new Discord.MessageEmbed()
-                .setColor('#ff0000')
-                .setTitle(`**You have been kicked from ${msg.guild.name}**`)
-                .setDescription( '**Moderator**: ' + msg.member.displayName + '\n**Reason**: ' + kickreason )
-                .setTimestamp()
+    //         const dmkickembed = new Discord.MessageEmbed()
+    //             .setColor('#ff0000')
+    //             .setTitle(`**You have been kicked from ${msg.guild.name}**`)
+    //             .setDescription( '**Moderator**: ' + msg.member.displayName + '\n**Reason**: ' + kickreason )
+    //             .setTimestamp()
 
-            mem.send(dmkickembed)
-            .then(() => {
-                mem.kick().then((member) => {
-                msg.channel.send(kickembed)
-                })
-            }).catch(() => {
-                msg.channel.send(cantkick);
-        });
-    }
+    //         mem.send(dmkickembed)
+    //         .then(() => {
+    //             mem.kick().then((member) => {
+    //             msg.channel.send(kickembed)
+    //             })
+    //         }).catch(() => {
+    //             msg.channel.send(cantkick);
+    //     });
+    // }
 
     if(msg.content.startsWith(prefix + 'ban')) {
 
---
+++ kick.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'punch',
+	description: 'punch command',
+	execute(msg, args) {
+
+        if (msg.channel.type == "dm") return;
+
+        const args = msg.content.split(' ').slice(2); 
+        const kickreason = args.join(' '); 
+        let mem= msg.mentions.members.first();
+
+        const noperm = new Discord.MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle('Oops!')
+            .setDescription("You can't use that command!")
+        
+        const nomemmber = new Discord.MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle('Oops!')
+            .setDescription("Give a member to kick!")
+
+        const noreason = new Discord.MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle('Oops!')
+            .setDescription("Give me a reason!")
+
+        const cantkick = new Discord.MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle('Oops!')
+            .setDescription("An errror occurred.")
+
+            if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(noperm);
+            if(!mem) return msg.channel.send(nomemmber)
+            if(!kickreason) return msg.channel.send(noreason)
+            
+            const kickembed = new Discord.MessageEmbed()
+                .setColor('#ff0000')
+                .setTitle('**Successfully kicked member**')
+                .setDescription( `**Kicked ${mem.displayName}.**` + '\n**Moderator**: ' + msg.member.displayName + '\n**Reason**: ' + kickreason )
+                .setTimestamp()
+
+            const dmkickembed = new Discord.MessageEmbed()
+                .setColor('#ff0000')
+                .setTitle(`**You have been kicked from ${msg.guild.name}**`)
+                .setDescription( '**Moderator**: ' + msg.member.displayName + '\n**Reason**: ' + kickreason )
+                .setTimestamp()
+
+            mem.send(dmkickembed)
+            .then(() => {
+                mem.kick().then((member) => {
+                msg.channel.send(kickembed)
+                })
+            }).catch(() => {
+                msg.channel.send(cantkick);
+        });
+    }
+};
