196bd4e399b086194dcf2ac88a96f8459e80ec3b
calvinjung <myadiuio@gmail.com>
2020-06-19 11:16:13 +0900

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,33 +1,21 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const { MessageEmbed } = require("discord.js");
 module.exports = {
-	name: 'avatar',
-	description: 'avatar command',
-	execute(msg, args) {
-        const user = msg.mentions.members.first()
-        const arguments = msg.content.split(' ').slice(1); 
-        const memberid = arguments.join(' '); 
-        if(!memberid && !user) {
-            const avatarEmbed = new Discord.MessageEmbed()
-                .setColor(`#006a4e`)
-                //.setAuthor(bot.users.cache.get(msg.author.id).tag, msg.author.displayAvatarURL())
-                .setImage(msg.author.displayAvatarURL({ dynamic:true }));
-            msg.channel.send(avatarEmbed);
-        }
-        if(user) {
-            const useravatarEmbed = new Discord.MessageEmbed()            
-                .setColor(`#006a4e`)
-                //.setAuthor(bot.users.cache.get(user.id).tag, bot.users.cache.get(user.id).displayAvatarURL())
-                .setImage(bot.users.cache.get(user.id).displayAvatarURL({ dynamic:true }));
-            msg.channel.send(useravatarEmbed);
-        }
-        if(memberid && !user && msg.guild.member(memberid)) {
-            const idavatarEmbed = new Discord.MessageEmbed() 
-                .setColor(`#006a4e`)
-                //.setAuthor(bot.users.cache.get(memberid).tag, bot.users.cache.get(memberid).displayAvatarURL())
-                .setImage(bot.users.cache.get(memberid).displayAvatarURL({ dynamic:true }));
-            msg.channel.send(idavatarEmbed);
-        } 
-        if(memberid && !user && !msg.guild.member(memberid) ) return msg.channel.send(":x: No results found.")
-	},
+  name: "avatar",
+  description: "Get your own or someone else's avatar",
+
+  run: async (bot, message, args) => {
+    let Embed = new MessageEmbed();
+    if (!message.mentions.users.first()) {
+      Embed.setTitle(`Your avatar!`);
+      Embed.setThumbnail(message.author.displayAvatarURL());
+      Embed.setColor(`RANDOM`);
+      return msg.channel.send(Embed);
+    } else {
+      let User = msg.mentions.members.first();
+      Embed.setTitle(`${bot.users.cache.get(User.id).tag}'s avatar!`);
+      Embed.setThumbnail(bot.users.cache.get(User.id).displayAvatarURL());
+      Embed.setColor(`RANDOM`);
+      return msg.channel.send(Embed);
+    }
+  },
 };
