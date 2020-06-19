6e319bc2662382b42c0c0d029929c8d543f456db
calvinjung <myadiuio@gmail.com>
2020-06-19 11:44:44 +0900

    adsf

--- index.js
+++ index.js
@@ -59,6 +59,10 @@ bot.on("message", async msg => {
     if(command == "coinflip") {
         bot.commands.get('coinflip').execute(msg, args);
     }
+    if(command == "help") {
+        bot.commands.get('help').execute(msg, args);
+    }
+
 
     if(msg.content == prefix + "ping") {
         const m = await msg.channel.send("Pong:");
@@ -226,16 +230,6 @@ bot.on("message", async msg => {
             .setTimestamp()
         msg.channel.send(embedembed)
 	}
-	
-    if(msg.content == prefix + "help"){
-        const helpEmbed = new Discord.MessageEmbed()
-	    .setColor('#0099ff')
-            .setTitle('**COMMAND LIST**')
-            .setDescription(`**Information commands**\n&botinfo : Gives bot info.\n&help : This command.\n&invite: Gives an invite to Memey Man server.\n\n**Main commands**\n&ping : Gives current ping.\n&meme : Gives a random meme.\n&urban : Gives urban dictionary definition.\n&punch : Punches a user!\n&hug: Hugs a user!\n&avatar: Gives your/someone's avatar.\n&8ball : Answers your question.\n&topic : Gives a chat topic.\n\n**Text commands**\n&repeat : Repeats your message.\n&embed : Puts your message in an embed.\n&xue : Xue hua piao piao bei feng...\n\n**Image commands**\n&cat: Gives a cat image.\n&dog: Gives a dog image.\n&fox: Gives a fox image.\n\n**Utility commands**\n&vote: Creates a vote.\n&coinflip : Flips a coin.\n&diceroll : Rolls a dice.\n\n**Moderator commands**\n&ban : Bans a user.\n&kick : Kicks a user.\n&purge : Deletes messages.\n&slowmode: Sets slowmode.`)
-        msg.reply("DM sent!")
-        msg.author.send(helpEmbed); 
-	    
-    }
 
     if(msg.content.startsWith(prefix + 'kick')) {
 
