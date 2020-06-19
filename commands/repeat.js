6118df9fb769b1161f8bba528f9a062aaab8495f
calvinjung <myadiuio@gmail.com>
2020-06-19 12:11:51 +0900

    Update repeat.js

--- commands/repeat.js
+++ commands/repeat.js
@@ -3,7 +3,7 @@ module.exports = {
 	name: 'repeat',
 	description: 'repeat command',
 	execute(msg, args) {
-        
+
         function getUserFromMention(mention) {
             if (!mention) return;
         
@@ -22,9 +22,11 @@ module.exports = {
         const args1 = msg.content.split(' ').slice(1);
         var repeatword = args1.join(' ')
         if(!repeatword) return msg.reply('Nothing to repeat!');
-        if(repeatword.includes(prefix + "kick")) return msg.reply("Don't try it!")
-        if(repeatword.includes(prefix + "ban")) return msg.reply("Don't try it!")
-        if(repeatword.includes(prefix + "purge")) return msg.reply("Don't try it!")   
+        if(repeatword.includes("&kick")) return msg.reply("Don't try it!")
+        if(repeatword.includes("&ban")) return msg.reply("Don't try it!")
+        if(repeatword.includes("&purge")) return msg.reply("Don't try it!")   
+        if(repeatword.includes("&slowmode")) return msg.reply("Don't try it!")  
+        if(repeatword.includes("&vote")) return msg.reply("Don't try it!")  
         if(repeatword.includes("@everyone")) return msg.reply("Don't try it!")
         if(repeatword.includes("@here")) return msg.reply("Don't try it!")
         if(user) return msg.channel.send("I can't mention user!")
