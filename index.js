c45ec426c0862bc18e6fca1c1e582d145ced5c58
calvinjung <myadiuio@gmail.com>
2020-06-19 14:32:12 +0900

    1

--- index.js
+++ index.js
@@ -68,6 +68,9 @@ bot.on("message", async msg => {
     if(command.startsWith("embed")) {
         bot.commands.get('embed').execute(msg, args);
     }
+    if(command.startsWith("slowmode")) {
+        bot.commands.get('slowmode').execute(msg, args);
+    }
 
     if(msg.content == prefix + "ping") {
         const m = await msg.channel.send("Pong:");
@@ -321,46 +324,46 @@ bot.on("message", async msg => {
         });
     }
 
-    if (msg.content.startsWith(prefix + 'slowmode')) {
+    // if (msg.content.startsWith(prefix + 'slowmode')) {
 
-        if (msg.channel.type == "dm") return;
+    //     if (msg.channel.type == "dm") return;
 
-        const noperm1 = new Discord.MessageEmbed()
-            .setColor('#FFFF00')
-            .setTitle('Oops!')
-            .setDescription("You can't use that command!")
+    //     const noperm1 = new Discord.MessageEmbed()
+    //         .setColor('#FFFF00')
+    //         .setTitle('Oops!')
+    //         .setDescription("You can't use that command!")
 
-        const nonum = new Discord.MessageEmbed()
-            .setColor('#FFFF00')
-            .setTitle('Oops!')
-            .setDescription("Number should be less than or equal to 21600.")
+    //     const nonum = new Discord.MessageEmbed()
+    //         .setColor('#FFFF00')
+    //         .setTitle('Oops!')
+    //         .setDescription("Number should be less than or equal to 21600.")
 
-        if(!msg.member.hasPermission("MANAGE_CHANNELS")) return msg.channel.send(noperm1)
+    //     if(!msg.member.hasPermission("MANAGE_CHANNELS")) return msg.channel.send(noperm1)
 
-        var str = msg.content.split(prefix + 'slowmode ');
-        var num = parseInt(str[1], 10);
-        if(num > 21600) return msg.channel.send(nonum)
-        if(!num) {
-            msg.channel.setRateLimitPerUser(num)
-            const slowmodeoff = new Discord.MessageEmbed()
-                .setColor('#EE0000')
-                .setTitle("Successfully turned off slowmode")
-                .setDescription(`Turned off slowmode.`)
-                .setTimestamp()
-            msg.channel.send(slowmodeoff)
-        }
-        else {
-            msg.channel.setRateLimitPerUser(num).then(() => {
-            const slowmode = new Discord.MessageEmbed()
-                .setColor('#EE0000')
-                .setTitle("Successfully set slowmode")
-                .setDescription(`Slowmode set to ${num}s.`)
-                .setTimestamp()
-            msg.channel.send(slowmode)
+    //     var str = msg.content.split(prefix + 'slowmode ');
+    //     var num = parseInt(str[1], 10);
+    //     if(num > 21600) return msg.channel.send(nonum)
+    //     if(!num) {
+    //         msg.channel.setRateLimitPerUser(num)
+    //         const slowmodeoff = new Discord.MessageEmbed()
+    //             .setColor('#EE0000')
+    //             .setTitle("Successfully turned off slowmode")
+    //             .setDescription(`Turned off slowmode.`)
+    //             .setTimestamp()
+    //         msg.channel.send(slowmodeoff)
+    //     }
+    //     else {
+    //         msg.channel.setRateLimitPerUser(num).then(() => {
+    //         const slowmode = new Discord.MessageEmbed()
+    //             .setColor('#EE0000')
+    //             .setTitle("Successfully set slowmode")
+    //             .setDescription(`Slowmode set to ${num}s.`)
+    //             .setTimestamp()
+    //         msg.channel.send(slowmode)
             
-            });
-        }
-    }
+    //         });
+    //     }
+    // }
     
     if(msg.content.startsWith(prefix + 'urban')) {
         const args = msg.content.split(' ').slice(1); 
