9086d926d4dc2308116f28e6f152c746b7fc7b8d
calvinjung <myadiuio@gmail.com>
2020-06-19 14:55:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -301,39 +301,6 @@ bot.on("message", async msg => {
                 msg.channel.send(cantban);
         });
     }
-    
-    // if(msg.content.startsWith(prefix + 'urban')) {
-    //     const args = msg.content.split(' ').slice(1); 
-    //     const searchword = args.join(' '); 
-    //     const noword = new Discord.MessageEmbed()
-    //         .setColor('#ffa000')
-    //         .setTitle('Urban command')
-    //         .setDescription('Usage: &urban [word]')
-    //     const toolong = new Discord.MessageEmbed()
-    //         .setColor('#ffa500')
-    //         .setTitle('Oops!')
-    //         .setDescription("Definition is too long. Can't send message because of discord embed character limit.")
-    //     if(!searchword) return msg.channel.send(noword);
-        
-    //     let image = "https://i.imgur.com/RFm5zMt.png";
-    //     let search = urban(searchword)
-    //         try {
-    //             search.first(res => {
-    //                 if(!res) return msg.channel.send(":x: No results found.");
-    //                 let { word, definition, example } = res;
-                        
-    //                     let embed = new Discord.MessageEmbed()
-    //                         .setColor('#ffa000')
-    //                         .setAuthor(`Urban Dictionary | ${word}`, image )
-    //                         .setDescription(`**Defintion:** ${definition || "No definition"}\n**Example:** ${example || "No Example"}`)
-    //                         //.setTimestamp()
-    //                         if( definition.length + word.length + example.length + 22 > 2048 ) return msg.channel.send(toolong)
-    //                         msg.channel.send(embed)                   
-    //             })
-    //         } catch(e) {
-    //             return msg.channel.send("Error")
-    //         }
-    //     }
 })
 
 bot.login("NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU");
