4ff740cc7d3270dc7076afe34a20b7e84c314f19
calvinjung <myadiuio@gmail.com>
2020-06-22 08:39:59 +0900

    1

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -6,7 +6,12 @@ module.exports = {
         const serverembed = new Discord.MessageEmbed()
             .setColor("#999900")
             .setTitle("Server info")
-            .setDescription(`**Server name**: ${msg.guild.name}\n**Member count**: ${msg.guild.memberCount}\n**Server owner**: ${msg.guild.owner}\n**Server ID**: ${msg.guild.id}`)
+            .addFields(
+                { name: 'Server name', value: msg.guild.name },
+                { name: 'Member count', value: msg.guild.memberCount },
+                { name: 'Server owner', value: msg.guild.owner },
+                { name: 'Server ID', value: msg.guild.id }
+            )
             .setTimestamp()
         msg.channel.send(serverembed)
 	},
