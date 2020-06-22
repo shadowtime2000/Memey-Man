3f27f2b4617c54a5fff25d74f7487678bc6263e7
calvinjung <myadiuio@gmail.com>
2020-06-22 16:56:59 +0900

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -5,7 +5,7 @@ module.exports = {
 	description: 'serverinfo command',
 	execute(msg, args) {
         var s = msg.guild.createdAt;
-        var s = toString(s)
+        s = toString(s)
         s = s.split('+')[0]
         const serverembed = new Discord.MessageEmbed()
             .setColor("#999900")
