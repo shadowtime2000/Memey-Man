d147da3e993ebc672d841f6d2103b1e6c37ad241
calvinjung <myadiuio@gmail.com>
2020-06-22 14:20:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ bot.on("message", async msg => {
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
         const avatar = await Canvas.loadImage(bot.users.cache.get(msg.author.id).displayAvatarURL({ format: 'png' }));
-        ctx.drawImage(avatar, 450, 130, 70, 70);
+        ctx.drawImage(avatar, 440, 120, 70, 70);
 
         const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'russia.jpg');
 
