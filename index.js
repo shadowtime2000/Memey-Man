fcd35d2135619788b9f8226b94879ec1d04c29f3
calvinjung <myadiuio@gmail.com>
2020-06-22 16:25:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -182,21 +182,21 @@ bot.on("message", async msg => {
             const avatarEmbed = new Discord.MessageEmbed()
                 .setColor(`#006a4e`)
                 .setAuthor(bot.users.cache.get(msg.author.id).tag, bot.users.cache.get(msg.author.id).displayAvatarURL({ format: 'png' }))
-                .setImage(msg.author.displayAvatarURL({ size: '512', format: 'png' }));
+                .setImage(msg.author.displayAvatarURL({ size: 512, format: 'png' }));
             msg.channel.send(avatarEmbed);
         }
         if(user) {
             const useravatarEmbed = new Discord.MessageEmbed()            
                 .setColor(`#006a4e`)
                 .setAuthor(bot.users.cache.get(user.id).tag, bot.users.cache.get(user.id).displayAvatarURL({ format: 'png' }))
-                .setImage(bot.users.cache.get(user.id).displayAvatarURL({ size: '512', format: 'png' }));
+                .setImage(bot.users.cache.get(user.id).displayAvatarURL({ size: 512, format: 'png' }));
             msg.channel.send(useravatarEmbed);
         }
         if(memberid && !user && msg.guild.member(memberid)) {
             const idavatarEmbed = new Discord.MessageEmbed() 
                 .setColor(`#006a4e`)
                 .setAuthor(bot.users.cache.get(memberid).tag, bot.users.cache.get(memberid).displayAvatarURL({ format: 'png' }))
-                .setImage(bot.users.cache.get(memberid).displayAvatarURL({ size: '512', format: 'png' }));
+                .setImage(bot.users.cache.get(memberid).displayAvatarURL({ size: 512, format: 'png' }));
             msg.channel.send(idavatarEmbed);
         } 
         if(memberid && !user && !msg.guild.member(memberid) ) return msg.channel.send(":x: No results found.")
