87e2bc49ea2ae86af8511348da334dc297801737
calvinjung <myadiuio@gmail.com>
2020-06-22 12:33:09 +0900

    Update amiajoke.js

--- commands/amiajoke.js
+++ commands/amiajoke.js
@@ -1,22 +1,21 @@
 const Discord = require('discord.js');
 const Canvas = require('canvas');
-module.exports = {
-	name: 'amiajoke',
-	description: 'image command',
-	execute(msg, args) {
-        async () => {
-            const canvas = Canvas.createCanvas(897, 601);
-            const ctx = canvas.getContext('2d');
+module.exports.run = async (msg, args) => {
+        const canvas = Canvas.createCanvas(897, 601);
+        const ctx = canvas.getContext('2d');
 
-            const background = await Canvas.loadImage('./amiajoke.jpg');
-            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        const background = await Canvas.loadImage('./amiajoke.jpg');
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-            const avatar = await Canvas.loadImage(msg.author.displayAvatarURL({ format: 'png' }));
-            ctx.drawImage(avatar, 480, 50, 330, 330);
+        const avatar = await Canvas.loadImage(msg.author.displayAvatarURL({ format: 'png' }));
+        ctx.drawImage(avatar, 480, 50, 330, 330);
 
-            const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'amiajoke.jpg');
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'amiajoke.jpg');
 
-            msg.channel.send(attachment)
-        }
-    },
+        msg.channel.send(attachment)
 };
+
+module.exports.help = {
+	name: 'amiajoke',
+    description: 'image command'
+}
