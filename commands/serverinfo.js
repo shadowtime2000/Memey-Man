63bd496ad1578f02e16d17f374d5f148a3e4872f
calvinjung <myadiuio@gmail.com>
2020-06-22 17:13:09 +0900

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -4,13 +4,6 @@ module.exports = {
 	name: 'serverinfo',
 	description: 'serverinfo command',
 	execute(msg, args) {
-        function capitalizeFirstLetter(string) {
-            return string.charAt(0).toUpperCase() + string.slice(1);
-        }
-
-        var s = msg.guild.verificationLevel
-        var temp = s.toString().toLowerCase().capitalizeFirstLetter();
-        
         const serverembed = new Discord.MessageEmbed()
             .setColor("#999900")
             .setTitle("**Server info**")
@@ -18,7 +11,6 @@ module.exports = {
             .addFields(
                 { name: 'Server name', value: msg.guild.name },
                 { name: 'Member count', value: msg.guild.memberCount },
-                { name: 'Verification level', value: temp },
                 { name: 'Server owner', value: msg.guild.owner },
                 { name: 'Server ID', value: msg.guild.id }
             )
