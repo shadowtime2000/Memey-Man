cfafb492fbf48ebd506862cab42cfc9aa0e344d5
calvinjung <myadiuio@gmail.com>
2020-06-22 10:19:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ bot.on("message", async msg => {
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
         const avatar = await Canvas.loadImage(bot.users.cache.get(msg.author.id).displayAvatarURL({ format: 'png' }));
-        ctx.drawImage(avatar, 50, 50, 200, 200);
+        ctx.drawImage(avatar, 50, 75, 200, 200);
 
         const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'amiajoke.jpg');
 
