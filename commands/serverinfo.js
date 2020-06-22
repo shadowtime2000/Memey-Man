057433a83133b77bee7a9ef79b9256b998a91d56
calvinjung <myadiuio@gmail.com>
2020-06-22 11:29:15 +0900

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -6,6 +6,7 @@ module.exports = {
         const serverembed = new Discord.MessageEmbed()
             .setColor("#999900")
             .setTitle("**Server info**")
+            .setThumbnail(msg.guild.iconURL())
             .addFields(
                 { name: 'Server name', value: msg.guild.name },
                 { name: 'Member count', value: msg.guild.memberCount },
