0439eb2024cee87d9673bc2b924d1efce360edd5
calvinjung <myadiuio@gmail.com>
2020-06-22 16:18:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -99,23 +99,6 @@ bot.on("message", async msg => {
         m.edit(`Pong: ${m.createdTimestamp - msg.createdTimestamp}ms`);
     }
 
-    // if(command.startsWith("repeat")) {
-    //     const user = msg.mentions.members.first();
-    //     const args1 = msg.content.split(' ').slice(1);
-    //     var repeatword = args1.join(' ')
-    //     if(!repeatword) return msg.reply('Nothing to repeat!');
-    //     if(repeatword.includes("&kick")) return msg.reply("Don't try it!")
-    //     if(repeatword.includes("&ban")) return msg.reply("Don't try it!")
-    //     if(repeatword.includes("&purge")) return msg.reply("Don't try it!")   
-    //     if(repeatword.includes("&slowmode")) return msg.reply("Don't try it!")  
-    //     if(repeatword.includes("&vote")) return msg.reply("Don't try it!")  
-    //     if(repeatword.includes("@everyone")) return msg.reply("Don't try it!")
-    //     if(repeatword.includes("@here")) return msg.reply("Don't try it!")
-    //     if(user) return msg.channel.send("I can't mention user!")
-    //     var repeatword = repeatword.replace(/@&|@!/g, "**Non-ping:** ")
-    //     msg.channel.send(repeatword)
-    // }
-
     if(command == "amiajoke") {
         const canvas = Canvas.createCanvas(897, 601);
         const ctx = canvas.getContext('2d');
