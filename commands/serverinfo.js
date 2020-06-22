07cc7910d022bf01776b399867ae2db5650d6380
calvinjung <myadiuio@gmail.com>
2020-06-22 16:46:29 +0900

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
+const { stringify } = require('querystring');
 module.exports = {
 	name: 'serverinfo',
 	description: 'serverinfo command',
 	execute(msg, args) {
-        const time = msg.guild.createdAt
-        time = time.split('+')[0]
+        var time = msg.guild.createdAt
+        var time = time.split('+')[0]
         const serverembed = new Discord.MessageEmbed()
             .setColor("#999900")
             .setTitle("**Server info**")
