9085e32c141b826c3e9431617ab8c0fe76abbd9f
calvinjung <myadiuio@gmail.com>
2020-06-24 01:44:36 +0900

    Update cityimage.js

--- commands/cityimage.js
+++ commands/cityimage.js
@@ -9,11 +9,11 @@ module.exports = {
         fetch(`http://api.teleport.org/api/urban_areas/slug:${cityname}/images`)
             .then(res => res.json())
             .then(json => {
-                const memeembed = new Discord.MessageEmbed()
-                    .setColor('#7cfc00')
-                    .setTitle("City picture of " + cityname + "!")
-                    .setImage(json.image)
-                msg.channel.send(memeembed)
+                // const memeembed = new Discord.MessageEmbed()
+                //     .setColor('#7cfc00')
+                //     .setTitle("City picture of " + cityname + "!")
+                //     .setImage(json.image)
+                msg.channel.send(json.image)
             });
 	},
 };
