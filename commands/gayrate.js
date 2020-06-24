3a452d93ce1012ce06e49eb42009da13d3261a9a
calvinjung <myadiuio@gmail.com>
2020-06-24 10:07:11 +0900

    1

---
+++ commands/gayrate.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'gayrate',
+	description: 'gayrate command',
+	execute(msg, args) {
+            const member = msg.mentions.members.first();
+            const rate = Math.random() * (100);
+            if(!member) {
+                const rateembed = new Discord.MessageEmbed()
+                    .setColor("#cccccc")
+                    .setTitle("Gayrate :gay-pride-flag:")
+                    .setDescription(`You are ${rate}% gay!`)
+                msg.channel.send(rateembed)
+            } if(member) {
+                const memberrateembed = new Discord.MessageEmbed()
+                    .setColor("#cccccc")
+                    .setTitle("Gayrate :gay-pride-flag:")
+                    .setDescription(`${member.displayName} is ${rate}% gay!`)
+                msg.channel.send(memberrateembed)
+            }
+        },
+};
