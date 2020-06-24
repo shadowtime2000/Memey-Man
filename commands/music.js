d4c3a9205db0388d8802b72bd1da0819883603f3
calvinjung <myadiuio@gmail.com>
2020-06-24 11:52:19 +0900

    Update music.js

--- commands/music.js
+++ commands/music.js
@@ -1,27 +1,32 @@
 const ytdl = require('ytdl-core');
 module.exports = {
-	name: 'music',
-	description: 'music command',
+	name: 'youtube',
+	description: 'youtube command',
 	execute(msg, args) {
             if (msg.channel.type !== 'text') return;
 
             const args1 = msg.content.split(' ').slice(1); 
             const musicurl = args1.join(' '); 
 
-            const voiceChannel = msg.member.voice.channel;
-
-            if (!voiceChannel) {
-                return message.reply('Please join a voice channel!');
+            if(!musicurl) {
+                const nosong = new Discord.MessageEmbed()
+                    .setColor('#505050')
+                    .setTitle('Youtube command')
+                    .setDescription('Usage: &youtube [youtube link]')
+                msg.channel.send(nosong)
             }
 
-            msg.reply(`Playing music now!`)
+            const voiceChannel = msg.member.voice.channel;
+
+            if (!voiceChannel) return msg.reply('Please join a voice channel!');
 
             voiceChannel.join().then(connection => {
                 const stream = ytdl(musicurl, { filter: 'audioonly' });
                 const dispatcher = connection.play(stream);
+                msg.reply(`Playing music now!`)
 
                 dispatcher.on('end', () => bot.leaveVoiceChannel(msg.member.voiceState.channelID));
             })
-            .catch(() => msg.reply(`Couldn't play music. Check if your link is valid youtube link!`));
+            process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
     },
 };
