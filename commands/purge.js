e27b1d0c83d72213e18ad81b67be4a9cfdf31f3e
calvinjung <myadiuio@gmail.com>
2020-06-26 20:41:48 +0900

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -34,12 +34,12 @@ module.exports = {
       
         msg.channel.messages.fetch({ limit: messageamount }).then(messages => {
             msg.channel.bulkDelete(messages).catch(error => console.log(error.stack))
-            .then(() => msg.channel.send(purge))
         })  
         .catch(() => msg.channel.send("An error occured.") )
-        .then(sentEmbed => {
-            sentEmbed.delete({ timeout: 5000 })
-            }
-        );
+        // .then(() => msg.channel.send(purge))
+        // .then(sentEmbed => {
+        //     sentEmbed.delete({ timeout: 5000 })
+        //     }
+        // );
 	},
 };
