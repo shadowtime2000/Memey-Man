a4d7dfa4f94e2a065705341693a8eab8d007861c
calvinjung <myadiuio@gmail.com>
2020-06-26 09:52:16 +0900

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,16 @@
 const Discord = require('discord.js');
+const fs = require('fs');
+
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+	bot.commands.set(command.name, command);
+}
+
 module.exports = {
 	name: 'reload',
 	description: 'reload command',
@@ -10,9 +20,9 @@ module.exports = {
         const command = msg.bot.commands.get(commandName)
 
         if (!command) return msg.channel.send(`There is no command with name \`${commandName}\`, ${msg.author}!`);
-        delete require.cache[require.resolve(`./${command.name}.js`)];
+        delete require.cache[require.resolve(`./commands/${command.name}.js`)];
         try {
-            const newCommand = require(`./${command.name}.js`);
+            const newCommand = require(`./commands/${command.name}.js`);
             msg.bot.commands.set(newCommand.name, newCommand);
             msg.channel.send(`Command \`${command.name}\` was reloaded!`);
         } catch (error) {
