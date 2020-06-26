036aa76f87de13fe63584acb87496b823b79a9c5
calvinjung <myadiuio@gmail.com>
2020-06-26 20:50:46 +0900

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -33,7 +33,7 @@ module.exports = {
 
       
         msg.channel.messages.fetch({ limit: messageamount }).then(messages => {
-            msg.channel.bulkDelete(messages).catch(error => msg.channel.send("An error occured."))
+            msg.channel.bulkDelete(messages).catch(error => msg.channel.send("I can only delete messages created within 14 days!"))
         })  
         // .then(() => msg.channel.send(purge))
         // .then(sentEmbed => {
