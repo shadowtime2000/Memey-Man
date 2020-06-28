da1b86ec067588526c76964c6ab953a5e339f7f1
calvinjung <myadiuio@gmail.com>
2020-06-29 01:14:04 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
-module.exports.run = async (msg, args) => {
+module.exports.execute = async (msg, args) => {
     const m = await msg.channel.send("Pong:");
     m.edit(`Pong: ${m.createdTimestamp - msg.createdTimestamp}ms`);
 };
 
-module.exports.help = {
+module.exports = {
     name: "ping",
     description: "ping"
   };
