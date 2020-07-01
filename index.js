bc960b63b231761881a03027841f04e94f987f1a
calvinjung <myadiuio@gmail.com>
2020-07-01 19:31:55 +0900

    1

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const Discord = require('discord.js');
-const Canvas = require('canvas');
-const ytdl = require('ytdl-core');
-const YouTube = require("discord-youtube-api");
 const Enmap = require("enmap");
 const fs = require('fs');
 
@@ -40,101 +37,101 @@ bot.on("message", async msg => {
     if (!cmd) return;
     cmd.run(bot, msg, args);
 
-    if(command.startsWith("play")) {
-        if (msg.channel.type !== 'text') return;
+    // if(command.startsWith("play")) {
+    //     if (msg.channel.type !== 'text') return;
 
-        const voiceChannel = msg.member.voice.channel;
+    //     const voiceChannel = msg.member.voice.channel;
 
-        const args1 = msg.content.split(' ').slice(1); 
-        const musicurl = args1.join(' '); 
+    //     const args1 = msg.content.split(' ').slice(1); 
+    //     const musicurl = args1.join(' '); 
 
-        const nosong = new Discord.MessageEmbed()
-            .setColor('#505050')
-            .setTitle('Play command')
-            .setDescription('Usage: &play [YouTube link or keyword]')
+    //     const nosong = new Discord.MessageEmbed()
+    //         .setColor('#505050')
+    //         .setTitle('Play command')
+    //         .setDescription('Usage: &play [YouTube link or keyword]')
 
-        const novc = new Discord.MessageEmbed()
-            .setColor('#505050')
-            .setTitle('Join a voice channel first!')
-            .setDescription('You have to join a voice channel before playing music.')
+    //     const novc = new Discord.MessageEmbed()
+    //         .setColor('#505050')
+    //         .setTitle('Join a voice channel first!')
+    //         .setDescription('You have to join a voice channel before playing music.')
 
-        if(!musicurl) return msg.channel.send(nosong)
+    //     if(!musicurl) return msg.channel.send(nosong)
 
-        if(msg.guild.me.voice.channel != msg.member.voice.channel && msg.guild.me.voice.channel) return msg.reply("You are not in the same voice channel with me!")
+    //     if(msg.guild.me.voice.channel != msg.member.voice.channel && msg.guild.me.voice.channel) return msg.reply("You are not in the same voice channel with me!")
 
-        if(ytdl.validateURL(musicurl) == false) {
+    //     if(ytdl.validateURL(musicurl) == false) {
 
-            const music = encodeURI(musicurl);
+    //         const music = encodeURI(musicurl);
 
-            let result
+    //         let result
 
-            try {
-                result = await youtube.searchVideos(music);
-            } catch (error) {
-                msg.channel.send(`:x: No results found. This might be an error.`)
-                console.log(error)
-            }
+    //         try {
+    //             result = await youtube.searchVideos(music);
+    //         } catch (error) {
+    //             msg.channel.send(`:x: No results found. This might be an error.`)
+    //             console.log(error)
+    //         }
 
-            const songInfo = await ytdl.getInfo(result.url);
-            const song = {
-                title: songInfo.title,
-                url: songInfo.video_url,
-                duration: songInfo.length_seconds
-            }; 
+    //         const songInfo = await ytdl.getInfo(result.url);
+    //         const song = {
+    //             title: songInfo.title,
+    //             url: songInfo.video_url,
+    //             duration: songInfo.length_seconds
+    //         }; 
 
-            if(!result) return msg.channel.send(":x: No results found.")
+    //         if(!result) return msg.channel.send(":x: No results found.")
 
-            var minute = parseInt(song.duration / 60); 
-            var second = song.duration % 60;
+    //         var minute = parseInt(song.duration / 60); 
+    //         var second = song.duration % 60;
 
-            const playing = new Discord.MessageEmbed()
-                .setColor('#505050')
-                .setTitle('Playing music!')
-                .setDescription(`Playing **${song.title}** now! :notes:`)
-                .setFooter(`Song duration: ${minute} minutes ${second} seconds`)
+    //         const playing = new Discord.MessageEmbed()
+    //             .setColor('#505050')
+    //             .setTitle('Playing music!')
+    //             .setDescription(`Playing **${song.title}** now! :notes:`)
+    //             .setFooter(`Song duration: ${minute} minutes ${second} seconds`)
 
-            voiceChannel.join().then(connection => {
-                const stream = ytdl(result.url, { filter: 'audioonly' });
-                const dispatcher = connection.play(stream);
-                msg.channel.send(playing)
+    //         voiceChannel.join().then(connection => {
+    //             const stream = ytdl(result.url, { filter: 'audioonly' });
+    //             const dispatcher = connection.play(stream);
+    //             msg.channel.send(playing)
     
-                dispatcher.on('finish', () => 
-                voiceChannel.leave()
-                );
-            })  
+    //             dispatcher.on('finish', () => 
+    //             voiceChannel.leave()
+    //             );
+    //         })  
             
-        }
+    //     }
         
-        else {
+    //     else {
 
-            if (!voiceChannel) return msg.channel.send(novc);
+    //         if (!voiceChannel) return msg.channel.send(novc);
 
-            const songInfo = await ytdl.getInfo(musicurl);
-            const song = {
-            title: songInfo.title,
-            url: songInfo.video_url,
-            duration: songInfo.length_seconds
-            };
+    //         const songInfo = await ytdl.getInfo(musicurl);
+    //         const song = {
+    //         title: songInfo.title,
+    //         url: songInfo.video_url,
+    //         duration: songInfo.length_seconds
+    //         };
 
-            var minute = parseInt(song.duration / 60); 
-            var second = song.duration % 60;
+    //         var minute = parseInt(song.duration / 60); 
+    //         var second = song.duration % 60;
 
-            const playing = new Discord.MessageEmbed()
-                .setColor('#505050')
-                .setTitle('Playing music!')
-                .setDescription(`Playing **${song.title}** now! :notes:`)
-                .setFooter(`Song duration: ${minute} minutes ${second} seconds`)
+    //         const playing = new Discord.MessageEmbed()
+    //             .setColor('#505050')
+    //             .setTitle('Playing music!')
+    //             .setDescription(`Playing **${song.title}** now! :notes:`)
+    //             .setFooter(`Song duration: ${minute} minutes ${second} seconds`)
 
-            voiceChannel.join().then(connection => {
-                const stream = ytdl(musicurl, { filter: 'audioonly' });
-                const dispatcher = connection.play(stream);
-                msg.channel.send(playing)
+    //         voiceChannel.join().then(connection => {
+    //             const stream = ytdl(musicurl, { filter: 'audioonly' });
+    //             const dispatcher = connection.play(stream);
+    //             msg.channel.send(playing)
 
-                dispatcher.on('finish', () => 
-                voiceChannel.leave()
-                );
-        })        
-    }}
+    //             dispatcher.on('finish', () => 
+    //             voiceChannel.leave()
+    //             );
+    //     })        
+    // }}
 })
 
 bot.login("NzAyMDY4NzI0OTU3NDQ2MTQ1.XqALgg.vyM6B7AAFi3fO8UBzaxmD9xz9gU");
