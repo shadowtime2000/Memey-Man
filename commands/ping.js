d4da1fbcb0b5744d7c01fc842d8f99391045ce63
calvinjung <myadiuio@gmail.com>
2020-07-02 01:30:33 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,5 +2,5 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 exports.run = async (bot, msg, args) => {
     const m = msg.channel.send("Please wait...")
-    .then((m) => m.edit(`Latency: ${m.createdTimestamp - msg.createdTimestamp}ms\nAPI latency: ${bot.ws.ping}`))
+    .then((m) => m.edit(`Latency: ${m.createdTimestamp - msg.createdTimestamp}ms\nAPI latency: ${bot.ws.ping}ms`))
 };
