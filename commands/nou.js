08863edb71e3d04e17cccf804e8b9447c533e316
calvinjung <myadiuio@gmail.com>
2020-07-01 19:22:11 +0900

    1

--- commands/nou.js
+++ commands/nou.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 const Canvas = require('canvas');
 exports.run = async (bot, msg, args) => {
-    const canvas = Canvas.createCanvas(897, 601);
+    const canvas = Canvas.createCanvas(640, 454);
     const ctx = canvas.getContext('2d');
 
-    const background = await Canvas.loadImage('./images/amiajoke.jpg');
+    const background = await Canvas.loadImage('./images/nou.jpg');
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
     const avatar = await Canvas.loadImage(bot.users.cache.get(msg.author.id).displayAvatarURL({ format: 'png' }));
-    ctx.drawImage(avatar, 480, 50, 330, 330);
+    ctx.drawImage(avatar, 37, 80, 200, 200);
 
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'amiajoke.jpg');
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'nou.jpg');
 
     msg.channel.send(attachment)
 };
--- commands/pogchamp.js
+++ commands/pogchamp.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 const Canvas = require('canvas');
 exports.run = async (bot, msg, args) => {
-    const canvas = Canvas.createCanvas(897, 601);
+    const canvas = Canvas.createCanvas(700, 394);
     const ctx = canvas.getContext('2d');
 
-    const background = await Canvas.loadImage('./images/amiajoke.jpg');
+    const background = await Canvas.loadImage('./images/pogchamp.jpg');
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
     const avatar = await Canvas.loadImage(bot.users.cache.get(msg.author.id).displayAvatarURL({ format: 'png' }));
-    ctx.drawImage(avatar, 480, 50, 330, 330);
+    ctx.drawImage(avatar, 270, 100, 170, 170);
 
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'amiajoke.jpg');
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'pogchamp.jpg');
 
     msg.channel.send(attachment)
 };
