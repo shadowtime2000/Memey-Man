3d05e6947fd7c8feb127a8c21eee3144395b39ee
calvinjung <myadiuio@gmail.com>
2020-07-01 19:27:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -40,35 +40,6 @@ bot.on("message", async msg => {
     if (!cmd) return;
     cmd.run(bot, msg, args);
 
-    if(command.startsWith('avatar')) {
-        var user = msg.mentions.members.first()
-        const arguments = msg.content.split(' ').slice(1); 
-        const memberid = arguments.join(' '); 
-        if(!memberid && !user) {
-            const avatarEmbed = new Discord.MessageEmbed()
-                .setColor(`#006a4e`)
-                .setAuthor(msg.author.tag, msg.author.displayAvatarURL({ format: 'png' }))
-                .setImage(msg.author.displayAvatarURL({ size: 1024, format: 'png' }));
-            msg.channel.send(avatarEmbed);
-        }
-        if(user) {
-            const useravatarEmbed = new Discord.MessageEmbed()            
-                .setColor(`#006a4e`)
-                .setAuthor(bot.users.cache.get(user.id).tag, bot.users.cache.get(user.id).displayAvatarURL({ format: 'png' }))
-                .setImage(bot.users.cache.get(user.id).displayAvatarURL({ size: 1024, format: 'png' }));
-            msg.channel.send(useravatarEmbed);
-        }
-        if(memberid && !user && msg.guild.member(memberid)) {
-            const idavatarEmbed = new Discord.MessageEmbed() 
-                .setColor(`#006a4e`)
-                .setAuthor(bot.users.cache.get(memberid).tag, bot.users.cache.get(memberid).displayAvatarURL({ format: 'png' }))
-                .setImage(bot.users.cache.get(memberid).displayAvatarURL({ size: 1024, format: 'png' }));
-            msg.channel.send(idavatarEmbed);
-        } 
-        if(memberid && !user && !msg.guild.member(memberid) ) return msg.channel.send(":x: No results found.")
-
-    }
-
     if(command.startsWith("play")) {
         if (msg.channel.type !== 'text') return;
 
