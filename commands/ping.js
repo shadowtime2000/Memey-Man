683b6e51241b016b807c7ee6d810e31743ce1267
calvinjung <myadiuio@gmail.com>
2020-07-01 19:15:50 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
 exports.run = async (bot, msg, args) => {
     const m = msg.channel.send("Pong:")
-    .then((sentMessage) => sentMessage.edit(`Pong: ${m.createdTimestamp - msg.createdTimestamp}ms`))
+    .then((m) => m.edit(`Pong: ${m.createdTimestamp - msg.createdTimestamp}ms`))
 };
