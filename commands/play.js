fbc1184e93a347ff8d27c6f8bb35c93e586ce796
calvinjung <myadiuio@gmail.com>
2020-07-01 20:27:00 +0900

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -3,7 +3,7 @@ const ytdl = require('ytdl-core');
 const YouTube = require('discord-youtube-api');
 exports.run = async (bot, msg, args) => {
 
-        const youtube = new YouTube('AIzaSyDTOmYVyZvnv7gSXM2TiHVH6FCSC9uqFCw');
+        const youtube = new YouTube('AIzaSyCz2eWaiih_tD1Rei1kj0hKvaV_TFwphYU');
 
         if (msg.channel.type !== 'text') return;
 
