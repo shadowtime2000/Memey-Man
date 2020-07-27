1132ed19fab707e943a8987f123347b91be2c2db
calvinjung <myadiuio@gmail.com>
2020-07-27 14:41:43 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ exports.run = async (bot, msg, args) => {
                                         description: '``&meme`` Gives a random meme.\n``&gayrate`` See how gay is someone\n``&hug`` Hug a user!\n``&punch`` Punch a user!\n``&topic`` Gives a chat topic\n``&urban`` Searchs a word at urban dictionary.'
                                 }
                         },
-                        '⚒️': {
+                        '🛠️': {
                                 embed: {
                                         color: "#1167b1",
                                         title: "Tool commands",
