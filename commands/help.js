bdbbbdd063f45474b31a214253d69f21ca337325
calvinjung <myadiuio@gmail.com>
2020-07-27 13:50:46 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,31 +1,39 @@
 const Discord = require('discord.js');
 const { ReactionCollector } = require("discord.js-collector");
 exports.run = async (bot, msg, args) => {
-        const botMessage = await msg.channel.send("Memey Man's commands!");
-        ReactionCollector.paginator({
+        const botMessage = await msg.channel.send('Memey Man\'s commands!')
+        ReactionCollector.menu({
                 botMessage,
                 user: msg,
-                pages: [
-                        new Discord.MessageEmbed({ 
-                                title: "Command list",
-                                description: "``Page 1`` Fun commands\n``Page 2`` Image commands\n``Page 3`` Text commands\n``Page 4`` Moderation commands" 
-                        }),
-                        new Discord.MessageEmbed({ 
-                                title: "Fun commands",
-                                description: "First page content..." 
-                        }),
-                        new Discord.MessageEmbed({ 
-                                title: "Image commands",
-                                description: "First page content..." 
-                        }),
-                        new Discord.MessageEmbed({ 
-                                title: "Text commands",
-                                description: "First page content..." 
-                        }),
-                        new Discord.MessageEmbed({ 
-                                title: "Moderation commands",
-                                description: "First page content..." 
-                        }),
-                ],
+                pages: {
+                        '😄': {
+                                embed: {
+                                        color: "#1167b1",
+                                        title: "Fun commands",
+                                        description: 'First page content, you can edit and put your custom embed.'
+                                }
+                        },
+                        '🎨': {
+                                embed: {
+                                        color: "#1167b1",
+                                        title: "Image commands",
+                                        description: 'e'
+                                }
+                        },
+                        '🔤': {
+                                embed: {
+                                        color: "#1167b1",
+                                        title: "Text commands",
+                                        description: 'e'
+                                }
+                        },
+                        '🔨': {
+                                embed: {
+                                        color: "#1167b1",
+                                        title: "Moderator commands",
+                                        description: 'e'
+                                }
+                        }
+                }
         });
 };
