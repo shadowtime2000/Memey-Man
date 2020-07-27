ee956f48987850663417684c1fe3b0a823277aa3
calvinjung <myadiuio@gmail.com>
2020-07-27 15:00:11 +0900

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,14 +1,20 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 exports.run = (bot, msg, args) => {
-    fetch('https://meme-api.herokuapp.com/gimme')
-        .then(res => res.json())
-        .then(json => {
-            const memeembed = new Discord.MessageEmbed()
-                .setColor('#7cfc00')
-                .setTitle(json.title)
-                .setImage(json.url)
-                .setFooter(`Subreddit: r/${json.subreddit}`)
-            msg.channel.send(memeembed)
-        });
+    function meme() {
+        fetch('https://meme-api.herokuapp.com/gimme')
+            .then(res => res.json())
+            .then(json => {
+                const memeembed = new Discord.MessageEmbed()
+                    .setColor('#7cfc00')
+                    .setTitle(json.title)
+                    .setImage(json.url)
+                    .setFooter(`Subreddit: r/${json.subreddit}`)
+                if(json.nsfw == true) return meme()
+                msg.channel.send(memeembed) 
+            });
+    }
+
+    meme()
+
 };
