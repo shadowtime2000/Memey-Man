c7d931f05aa40ed721c12c6e447bbb9f1059c5b7
calvinjung <myadiuio@gmail.com>
2020-07-27 13:46:50 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,23 +6,23 @@ exports.run = async (bot, msg, args) => {
                 botMessage,
                 user: msg,
                 pages: [
-                        new MessageEmbed({ 
+                        new Discord.MessageEmbed({ 
                                 title: "Command list",
                                 description: "``Page 1`` Fun commands\n``Page 2`` Image commands\n``Page 3`` Text commands\n``Page 4`` Moderation commands" 
                         }),
-                        new MessageEmbed({ 
+                        new Discord.MessageEmbed({ 
                                 title: "Fun commands",
                                 description: "First page content..." 
                         }),
-                        new MessageEmbed({ 
+                        new Discord.MessageEmbed({ 
                                 title: "Image commands",
                                 description: "First page content..." 
                         }),
-                        new MessageEmbed({ 
+                        new Discord.MessageEmbed({ 
                                 title: "Text commands",
                                 description: "First page content..." 
                         }),
-                        new MessageEmbed({ 
+                        new Discord.MessageEmbed({ 
                                 title: "Moderation commands",
                                 description: "First page content..." 
                         }),
