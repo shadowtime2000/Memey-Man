bc17f652ec891b06dc1fc190a441c0d8eabe9d7a
calvinjung <myadiuio@gmail.com>
2020-07-29 10:32:50 +0900

    Update guess.js

--- commands/guess.js
+++ commands/guess.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const searchByImage = require("reverse-image-search-google");
 exports.run = (bot, msg, args) => {
-    var searchpic = msg.attachments.first()
+    var searchpic = msg.attachments.first().url
 
     const doSomething = (results) => {
         console.log(results)
