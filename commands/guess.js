e9f7436741092454e10cfe9d11ee8b33dbf697cd
calvinjung <myadiuio@gmail.com>
2020-07-29 10:42:36 +0900

    Update guess.js

--- commands/guess.js
+++ commands/guess.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js');
 const searchByImage = require("search-image");
 exports.run = (bot, msg, args) => {
 
-    searchByImage("i.ebayimg.com/00/s/OTAwWDkwMA==/z/3G8AAOSwzoxd80XB/$_83.JPG").then((res) => console.log(res));
+    try {
+        searchByImage("i.ebayimg.com/00/s/OTAwWDkwMA==/z/3G8AAOSwzoxd80XB/$_83.JPG").then((res) => console.log(res));
+    } catch (error) {
+        console.log(error)
+    }
 
 };
