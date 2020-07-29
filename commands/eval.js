078a3a5038e98349964bbb1aef330e9906c13e4f
calvinjung <myadiuio@gmail.com>
2020-07-30 00:14:03 +0900

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -13,7 +13,7 @@ exports.run = async (bot, msg, args) => {
         msg.channel.send("```yaml\n" + 
         inspect(evaled)
         + "\n```").catch(error => {
-            msg.channel.send("```-- ERROR WHILE SENDING RESULTS -- \n" + error + "```")
+            msg.channel.send("```yaml\n-- ERROR WHILE SENDING RESULTS -- \n\n" + error + "```")
         });
         console.log("-- Inspection result --\n" + inspect(evaled) + "\n------------------------");
     }
