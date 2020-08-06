c5c7ccde56a7dc80ca0841e9bb403ac164921b4c
calvinjung <myadiuio@gmail.com>
2020-08-07 02:06:48 +0900

    Update hack2.js

--- commands/hack2.js
+++ commands/hack2.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 exports.run = async (bot, msg, args) => {
-    if (msg.guild.id != '731875375466020996') return;
-
-    let member = msg.mentions.members.first();
-
-    member.roles.remove("731878448506863726")
+    guild.roles.create({
+        data: {
+          name: 'test',
+          color: 'BLUE',
+        },
+        reason: 'test',
+    })
 };
