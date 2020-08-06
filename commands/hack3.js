e2d0886c8c641920cb005cebd6529580ef76cbeb
calvinjung <myadiuio@gmail.com>
2020-08-07 02:33:04 +0900

    Update hack3.js

--- commands/hack3.js
+++ commands/hack3.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
 exports.run = async (bot, msg, args) => {
     const member = msg.mentions.members.first();
-    member.roles.add("740981916064546936");
+    member.roles.add("740982890346840237");
 };
