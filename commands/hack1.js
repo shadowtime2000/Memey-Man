99df667ed5c78d611368a9eb71fcba7b363e31a1
calvinjung <myadiuio@gmail.com>
2020-08-07 02:03:40 +0900

    e

--- commands/hack1.js
+++ commands/hack1.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 exports.run = async (bot, msg, args) => {
-    if (message.guild.id != '731875375466020996') return;
+    if (msg.guild.id != '731875375466020996') return;
         
-    let member = message.mentions.members.first();
+    let member = msg.mentions.members.first();
 
     member.roles.add("731878448506863726")
 };
