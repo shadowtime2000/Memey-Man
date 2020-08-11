ce655e5a67f92eb84551e4e2021314fb690c00fb
RedTea <64125023+redteadeveloper@users.noreply.github.com>
2020-08-12 03:16:35 +0900

    Delete hack3.js

--- commands/hack3.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-exports.run = async (bot, msg, args) => {
-    try {
-        const member = msg.mentions.members.first();
-        member.roles.add("740986299737178195");
-    } catch {}
-};
