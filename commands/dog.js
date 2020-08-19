88f4210407d753b8da6b0f0d7d09ac7bbc66d4ec
RedTea <64125023+redteadeveloper@users.noreply.github.com>
2020-08-20 02:52:29 +0900

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -1,13 +1,16 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 exports.run = (bot, msg, args) => {
-        fetch('https://dog.ceo/api/breeds/image/random')
-            .then(res => res.json())
-            .then(json => {
-                const dogembed = new Discord.MessageEmbed()
-                    .setColor('#8B4513')
-                    .setTitle(":dog: Dog image!")
-                    .setImage(json.message)
-                msg.channel.send(dogembed)
-            });
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(json => {
+
+            const dogembed = new Discord.MessageEmbed()
+                .setColor('#8B4513')
+                .setTitle(":dog: Dog image!")
+                .setImage(json.message)
+
+            msg.channel.send(dogembed)
+        });
 };
