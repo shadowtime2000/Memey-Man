5b74b428335f23500a65bc2b06b48410d590c6d0
RedTea <64125023+redteadeveloper@users.noreply.github.com>
2020-08-20 02:43:54 +0900

    Update diceroll.js

--- commands/diceroll.js
+++ commands/diceroll.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 exports.run = (bot, msg, args) => {
+
     var facts = ["1", "2", "3", "4", "5", "6"]
     var fact = Math.floor(Math.random() * facts.length);     const coinembed = new Discord.MessageEmbed()
         .setColor('#9c51b6')
@@ -7,4 +8,5 @@ exports.run = (bot, msg, args) => {
         .setDescription( 'Result: ' + facts[fact] )
         .setTimestamp()
     msg.channel.send(coinembed)
+
 };
