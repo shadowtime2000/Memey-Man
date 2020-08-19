143ada9e3b8f24f35fe2d290509b7dbe0c09e054
RedTea <64125023+redteadeveloper@users.noreply.github.com>
2020-08-20 02:59:32 +0900

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,13 +1,26 @@
 const Discord = require('discord.js');
 exports.run = (bot, msg, args) => {
-        const args1 = msg.content.split(' ').slice(1); 
-        const question = args1.join(' '); 
-        var facts = ["Yes.", "Ask me later.", "No.", "I don't know", "Of course.", "Never.", "Maybe.", "Hmm...", "Excuse me?"];
-        var fact = Math.floor(Math.random() * facts.length);
-        const ballembed = new Discord.MessageEmbed()
-            .setColor('#9c51b6')
-            .setTitle('**8ball**')
-            .setDescription( '**Your question**: ' + question + '\n**My answer**: ' + facts[fact] )
-            .setTimestamp() 
-        msg.channel.send(ballembed);
+
+    const args1 = msg.content.split(' ').slice(1); 
+    const question = args1.join(' '); 
+
+    var answers = ["Yes.",
+                 "Ask me later.", 
+                 "No.", 
+                 "I don't know",
+                 "Of course.",
+                 "Never.", 
+                 "Maybe.",
+                 "Hmm...",
+                 "Excuse me?"];
+
+    var answer = Math.floor(Math.random() * answers.length);
+
+    const ballembed = new Discord.MessageEmbed()
+        .setColor('#9c51b6')
+        .setTitle('**8ball**')
+        .setDescription( '**Your question**: ' + question + '\n**My answer**: ' + answers[answer] )
+        .setTimestamp() 
+
+    msg.channel.send(ballembed);
 };
