441fc43391ed72dff3e578f4e7d27d39fa3d0930
RedTea <64125023+redteadeveloper@users.noreply.github.com>
2020-08-20 03:04:24 +0900

    Update fox.js

--- commands/fox.js
+++ commands/fox.js
@@ -5,12 +5,14 @@ exports.run = (bot, msg, args) => {
     fetch('https://randomfox.ca/floof/?ref=public-apis')
     .then(res => res.json())
     .then(json => {
+
         const foxembed = new Discord.MessageEmbed()
             .setColor('#8B4513')
             .setTitle(":fox: Fox image!")
             .setImage(json.image)
 
         msg.channel.send(foxembed)
+
     });
 
 };
