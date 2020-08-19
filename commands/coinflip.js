ec30c19f298ac87a0872774ff8a32d926f1c273c
RedTea <64125023+redteadeveloper@users.noreply.github.com>
2020-08-20 02:43:29 +0900

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 exports.run = (bot, msg, args) => {
-        var facts = ["Head", "tail"]
-        var fact = Math.floor(Math.random() * facts.length);
-        const coinembed = new Discord.MessageEmbed()
-            .setColor('#9c51b6')
-            .setTitle('Coin flip')
-            .setDescription( 'Result: ' + facts[fact] )
-            .setTimestamp()
-        msg.channel.send(coinembed)
+
+    var facts = ["Head", "tail"]
+    var fact = Math.floor(Math.random() * facts.length);
+    const coinembed = new Discord.MessageEmbed()
+        .setColor('#9c51b6')
+        .setTitle('Coin flip')
+        .setDescription( 'Result: ' + facts[fact] )
+        .setTimestamp()
+    msg.channel.send(coinembed)
 
 };
