b63870106dc134cc6100a78c2494123604770eb6
RedTea <64125023+redteadeveloper@users.noreply.github.com>
2020-08-20 02:45:44 +0900

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,15 +1,18 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 exports.run = (bot, msg, args) => {
-        const infoEmbed = new Discord.MessageEmbed()
-	    .setColor('#0099ff')
-			.setTitle('**Bot info**')
-			.setThumbnail('https://i.imgur.com/XiiqQWn.png')
-			.addFields(
-                { name: 'Bot name', value: 'Memey Man' },
-                { name: 'Developer', value: 'RedTea#8520' },
-                { name: 'Bot prefix', value: '&' },
-            )
-            .setFooter('Type &help to get help!')
-	    msg.channel.send(infoEmbed);
+
+    const infoEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('**Bot info**')
+	.setThumbnail('https://i.imgur.com/XiiqQWn.png')
+	.addFields(
+            { name: 'Bot name', value: 'Memey Man' },
+            { name: 'Developer', value: 'RedTea#8520' },
+            { name: 'Bot prefix', value: '&' },
+        )
+        .setFooter('Type &help to get help!')
+
+    msg.channel.send(infoEmbed);
+
 };
