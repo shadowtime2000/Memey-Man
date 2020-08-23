34babfe6e12bca1f81985e8122a474e2e3f38457
calvinjung <myadiuio@gmail.com>
2020-08-23 14:29:11 +0900

    Create dadjoke.js

---
+++ commands/dadjoke.js
@@ -0,0 +1,24 @@
+const { DiscordAPIError } = require("discord.js");
+
+const Discord = require('discord.js');
+exports.run = (bot, msg, args) => {
+	async function fetchRedditData() {
+
+    const res = await fetch("https://www.reddit.com/r/dadjokes/random.json");
+        const data = await res.json();
+    
+        const title = data[0].data.children[0].data.title
+        const text = data[0].data.children[0].data.selftext
+        const author = data[0].data.children[0].data.author
+
+        const jokeembed = new Discord.MessageEmbed()
+            .setColor(`#9c51b6'`)
+            .setTitle(title)
+            .setDescription(text)
+            .setFooter(`Post by u/${author}`)
+
+        msg.channel.send(jokeembed)
+    }
+    
+    fetchRedditData()
+};
