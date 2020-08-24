99c78138a54fb87ed2e534e53371944353aff157
calvinjung <myadiuio@gmail.com>
2020-08-24 17:08:08 +0900

    Update wiki.js

--- commands/wiki.js
+++ commands/wiki.js
@@ -2,11 +2,20 @@ const Discord = require('discord.js');
 const fetch = require('node-fetch');
 exports.run = async (bot, msg, args) => {
 
+    const nosearch = new Discord.MessageEmbed()
+        .setColor("#FFA500")
+        .setTitle("Invalid argument")
+        .setDescription("&wiki [search word]")
+        .setFooter("You didn't provide a search word.")
+
     const args1 = msg.content.split(' ').slice(1);
     const search = args1.join(' ');
+
+    if (!search) return msg.channel.send(nosearch)
+
     const searchword = encodeURI(search)
 
-    const res = await fetch("https://en.wikipedia.org/api/rest_v1/page/summary/Stack_Overflow" + searchword);
+    const res = await fetch("https://en.wikipedia.org/api/rest_v1/page/summary/" + searchword);
     const data = await res.json();
 
     const title = data.title;
