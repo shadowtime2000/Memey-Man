fae39ab8ce3b2f3ac1dbfc11b98e38abebdd5d73
calvinjung <myadiuio@gmail.com>
2020-08-26 15:26:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.on("message", async msg => {
     const prefixmap = await guildprefix.findOne({ serverid: msg.guild.id }) || { prefix: "&" }
     const prefix = prefixmap.prefix
 
-    module.exports = { prefix: prefix };
+    module.exports = { prefix: `${prefix}` };
 
     if (msg.author.bot) return;
     if (msg.content.indexOf(prefix) !== 0) return;
