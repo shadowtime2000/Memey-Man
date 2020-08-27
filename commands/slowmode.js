e69270e79b0a5dbf2f1cef5c9732802f09cf0aa5
calvinjung <myadiuio@gmail.com>
2020-08-27 14:43:29 +0900

    Update slowmode.js

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const ms = require('ms')
 exports.run = (bot, msg, args) => {
 
     if (msg.channel.type == "dm") return;
@@ -14,34 +15,20 @@ exports.run = (bot, msg, args) => {
         .setDescription("Number should be less than or equal to 21600.")
 
     if(!msg.member.hasPermission("MANAGE_CHANNELS")) return msg.channel.send(noperm1)
+
     var str = msg.content.split('&slowmode ');
-    var num = parseInt(str[1], 10);
+    var num = parseInt(ms(str))/1000
 
     if(num > 21600) return msg.channel.send(nonum)
 
-    if (!num) {
+    msg.channel.setRateLimitPerUser(num)
 
-        msg.channel.setRateLimitPerUser(num)
-        const slowmodeoff = new Discord.MessageEmbed()
-            .setColor('#73DB6A')
-            .setTitle("Successfully turned off slowmode")
-            .setDescription(`Turned off slowmode.`)
-            .setFooter('Slowmode time unit is seconds')
-            .setTimestamp()
-        msg.channel.send(slowmodeoff)
-
-    } else {
-
-        msg.channel.setRateLimitPerUser(num)
-        const slowmodey = new Discord.MessageEmbed()
-            .setColor('#73DB6A')
-            .setTitle("Successfully set slowmode")
-            .setDescription(`Slowmode set to ${num}s.`)
-            .setFooter('Slowmode time unit is seconds')
-            .setTimestamp()
-
-        msg.channel.send(slowmodey)
-
-    }
+    const slowmodey = new Discord.MessageEmbed()
+        .setColor('#FF0000')
+        .setTitle("Successfully set slowmode")
+        .setDescription(`Slowmode set to ${str}.`)
+        .setTimestamp()
+        
+    msg.channel.send(slowmodey)
     
 };
