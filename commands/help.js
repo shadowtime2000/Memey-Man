7f9ca48b2eea62efd041a705226a33ed3c39ef2f
calvinjung <myadiuio@gmail.com>
2020-08-27 10:31:18 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,8 @@ exports.run = async (bot, msg, args) => {
                     title: "Fun commands",
                     description: '``meme`` Gets a random meme.\n``dadjoke`` Gets a random dad joke\n``gayrate`` See how gay is someone\n``hug`` Hug a user!\n``punch`` Punch a user!\n``topic`` Gives a chat topic\n``urban`` Searches something on urban dictionary.',
                     footer: {
-                        text: "This server's prefix is ``" + prefix + "``"
+                        iconURL: "https://i.imgur.com/XiiqQWn.png",
+                        text: "This server's prefix is " + prefix + "."
                     }
                 }
             },
@@ -25,7 +26,8 @@ exports.run = async (bot, msg, args) => {
                     title: "Tool commands",
                     description: '``ping`` Gets bot ping.\n``prefix`` Sets bot prefix.\n``wiki`` Searches something on Wikipedia.\n``avatar`` Gives user\'s avatar.\n``translate`` Translates text to English.\n``poll`` Creates a poll.\n``serverinfo`` Gives server info.',
                     footer: {
-                        text: "This server's prefix is ``" + prefix + "``"
+                        iconURL: "https://i.imgur.com/XiiqQWn.png",
+                        text: "This server's prefix is " + prefix + "."
                     }
                 }
             },
@@ -36,7 +38,8 @@ exports.run = async (bot, msg, args) => {
                     title: "Image commands",
                     description: '``dog`` Gives a dog image.\n``fox`` Gives a fox image.\n``amiajoke`` Am I a joke to you?\n``pogchamp`` Pogchamp!\n``nou`` No u\n``russia`` Mother Russia!',
                     footer: {
-                        text: "This server's prefix is ``" + prefix + "``"
+                        iconURL: "https://i.imgur.com/XiiqQWn.png",
+                        text: "This server's prefix is " + prefix + "."
                     }
                 }
             },
@@ -47,7 +50,8 @@ exports.run = async (bot, msg, args) => {
                     title: "Text commands",
                     description: '``xue`` Xue hua piao piao bei feng piao piao',
                     footer: {
-                        text: "This server's prefix is ``" + prefix + "``"
+                        iconURL: "https://i.imgur.com/XiiqQWn.png",
+                        text: "This server's prefix is " + prefix + "."
                     }
                 }
             },
@@ -58,7 +62,8 @@ exports.run = async (bot, msg, args) => {
                     title: "Moderator commands",
                     description: '``kick`` Kicks a user.\n``ban`` Bans a user.\n``purge`` Deletes messages.\n``slowmode`` Sets slowmode.',
                     footer: {
-                        text: "This server's prefix is ``" + prefix + "``"
+                        iconURL: "https://i.imgur.com/XiiqQWn.png",
+                        text: "This server's prefix is " + prefix + "."
                     }
                 }
             }
