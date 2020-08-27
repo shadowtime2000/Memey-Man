28c270c7724e4c471e727365e1b2fd030458a174
calvinjung <myadiuio@gmail.com>
2020-08-27 14:41:13 +0900

    Update slowmode.js

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -1,34 +1,47 @@
 const Discord = require('discord.js');
-const ms = require('ms')
 exports.run = (bot, msg, args) => {
 
     if (msg.channel.type == "dm") return;
 
     const noperm1 = new Discord.MessageEmbed()
-        .setColor('#FF665B')
+        .setColor('#FFa500')
         .setTitle('Missing permissions')
         .setDescription("You need ``Manage channels`` permission to use this command.")
 
     const nonum = new Discord.MessageEmbed()
-        .setColor('#FF665B')
+        .setColor('#FFa500')
         .setTitle('Invalid argument')
         .setDescription("Number should be less than or equal to 21600.")
 
     if(!msg.member.hasPermission("MANAGE_CHANNELS")) return msg.channel.send(noperm1)
-
     var str = msg.content.split('&slowmode ');
-    var num = parseInt(ms(str))/1000
+    var num = parseInt(str[1], 10);
 
     if(num > 21600) return msg.channel.send(nonum)
 
-    msg.channel.setRateLimitPerUser(num)
+    if (!num) {
 
-    const slowmodey = new Discord.MessageEmbed()
-        .setColor('#FF0000')
-        .setTitle("Successfully set slowmode")
-        .setDescription(`Slowmode set to ${str}.`)
-        .setTimestamp()
-        
-    msg.channel.send(slowmodey)
+        msg.channel.setRateLimitPerUser(num)
+        const slowmodeoff = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle("Successfully turned off slowmode")
+            .setDescription(`Turned off slowmode.`)
+            .setFooter('Slowmode time unit is seconds')
+            .setTimestamp()
+        msg.channel.send(slowmodeoff)
+
+    } else {
+
+        msg.channel.setRateLimitPerUser(num)
+        const slowmodey = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle("Successfully set slowmode")
+            .setDescription(`Slowmode set to ${num}s.`)
+            .setFooter('Slowmode time unit is seconds')
+            .setTimestamp()
+
+        msg.channel.send(slowmodey)
+
+    }
     
 };
