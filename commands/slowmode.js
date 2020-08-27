3e9f36d74406936760d721b5945ddf2be665a5dd
calvinjung <myadiuio@gmail.com>
2020-08-27 12:24:14 +0900

    Update slowmode.js

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -17,31 +17,30 @@ exports.run = (bot, msg, args) => {
     if(!msg.member.hasPermission("MANAGE_CHANNELS") && msg.author.id != "611396886418685982") return msg.channel.send(noperm1)
 
     var str = msg.content.split('&slowmode ');
-    
-    if(num > 21600) return msg.channel.send(nonum)
+    var num = ms(str)
 
-    if (!num) {
+    if(num/1000 > 21600) return msg.channel.send(nonum)
 
-        msg.channel.setRateLimitPerUser(ms(str)*1000)
+    if (num == undefined) {
+
+        msg.channel.setRateLimitPerUser("0")
 
         const slowmodeoff = new Discord.MessageEmbed()
             .setColor('#FF0000')
             .setTitle("Successfully turned off slowmode")
             .setDescription(`Turned off slowmode.`)
-            .setFooter('Slowmode time unit is seconds')
             .setTimestamp()
 
         msg.channel.send(slowmodeoff)
 
     } else {
 
-        msg.channel.setRateLimitPerUser(num)
+        msg.channel.setRateLimitPerUser(toString(num/1000))
 
         const slowmodey = new Discord.MessageEmbed()
             .setColor('#FF0000')
             .setTitle("Successfully set slowmode")
-            .setDescription(`Slowmode set to ${num}s.`)
-            .setFooter('Slowmode time unit is seconds')
+            .setDescription(`Slowmode set to ${num/1000}s.`)
             .setTimestamp()
             
         msg.channel.send(slowmodey)
