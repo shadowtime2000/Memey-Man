30494dd9053640580042c6698f9682ebad4f33c5
calvinjung <myadiuio@gmail.com>
2020-08-28 19:12:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -45,37 +45,12 @@ bot.on('guildMemberAdd', async member => {
 
     if (member.guild.id != `717895996155101244`) return
 
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
-    if (!channel) return;
-
-    const canvas = Canvas.createCanvas(700, 250);
-    const ctx = canvas.getContext('2d');
-
-    const background = await Canvas.loadImage('./images/wallpaper.png');
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-    ctx.strokeStyle = '#74037b';
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-    ctx.font = '28px sans-serif';
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText('Welcome to this server,', canvas.width / 2.5, canvas.height / 2.88);
-
-    ctx.font = applyText(canvas, `${member.displayName}`);
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.6);
-
-    ctx.beginPath();
-    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.clip();
-
-    const avatar = await Canvas.loadImage(member.guild.iconURL({ format: 'png' }));
-    ctx.drawImage(avatar, 25, 25, 200, 200);
-
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-
-    channel.send(`Welcome to the server, ${member}!`, attachment);
+    const welcomeembed = new Discord.MessageEmbed()
+        .setColor("#800080")
+        .setAuthor(member, member.user.displayAvatarURL({ format: 'png' }))
+        .setDescription("Welcome to " + member.guild.name + "! You're the " + member.guild.memberCount + " member. Don't forget to read the rules!")
+        .setFooter(member.guild.name)
+        .setTimestamp()
 
 });
 
