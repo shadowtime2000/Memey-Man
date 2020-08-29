f5696e9201cd040b8e97c200e4223ada655f2b80
calvinjung <myadiuio@gmail.com>
2020-08-29 18:56:47 +0900

    yikes

--- commands/draw.js
+++ commands/draw.js
@@ -10,7 +10,7 @@ exports.run = async (bot, msg, args) => {
         .setColor("#1167b1")
         .setTitle("Draw command")
         .addFields(
-            { name: 'Available options', value: "``" + prefix + "draw pog``\n``" + prefix + "draw kekw``"}
+            { name: 'Available options', value: "``" + prefix + "draw pog``\n``" + prefix + "draw kekw``\n``" + prefix + "draw yikes``"}
         )
 
     if(!menu) return msg.channel.send(menuembed)
@@ -73,7 +73,7 @@ exports.run = async (bot, msg, args) => {
             const noamountk = new Discord.MessageEmbed()
                 .setColor("#FF665B")
                 .setTitle("Invalid argument")
-                .setDescription(prefix + "draw pog [amount]")
+                .setDescription(prefix + "draw kekw [amount]")
                 .setFooter("You did't provide the number of kekw.")
             return msg.channel.send(noamountk)
         }
@@ -118,6 +118,56 @@ exports.run = async (bot, msg, args) => {
 
         await msg.channel.send(attachment)
 
-    } else {}
+    } else if (menu == "yikes") {
+
+        if(!arguments[1]) {
+            const noamountk = new Discord.MessageEmbed()
+                .setColor("#FF665B")
+                .setTitle("Invalid argument")
+                .setDescription(prefix + "draw yikes [amount]")
+                .setFooter("You did't provide the number of kekw.")
+            return msg.channel.send(noamountk)
+        }
+
+        if(isNaN(arguments[1])) {
+            const nonumk = new Discord.MessageEmbed()
+                .setColor("#FF665B")
+                .setTitle("Invalid argument")
+                .setDescription("Amount of yikes should be a number.")
+            return msg.channel.send(nonumk)
+        }
+
+        if(amount<0) {
+            const smallnumk = new Discord.MessageEmbed()
+                .setColor("#FF665B")
+                .setTitle("Invalid argument")
+                .setDescription("Amount of yikes should be bigger than or equal to 1.")
+            return msg.channel.send(smallnumk)
+        }
+
+        if(amount>100) {
+            const bignumk = new Discord.MessageEmbed()
+                .setColor("#FF665B")
+                .setTitle("Invalid argument")
+                .setDescription("Amount of yikes should be smaller than or equal to 100.")
+            return msg.channel.send(bignumk)
+        }
+
+        const pog = await loadImage("./images/yikes.png")
+        const rows = Math.ceil(amount / 10)
+        const canvas = createCanvas(pog.width * (rows > 1 ? 10 : amount), pog.height*rows)
+        const ctx = canvas.getContext("2d")
+        let width = 0
+        for (var i = 0; i < amount; i++) {
+            const row = Math.ceil((i+1)/10)
+            ctx.drawImage(pog, width, pog.height * (row - 1))
+            if((width + pog.width) === (pog.width * (rows > 1 ? 10 : amount))) width = 0
+            else width += pog.width
+        }
+
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'pog.png');
+
+        await msg.channel.send(attachment)
+    }
 
 };
