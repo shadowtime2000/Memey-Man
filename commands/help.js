4298722748c00e4678401f956b4fca68fa78dca9
calvinjung <myadiuio@gmail.com>
2020-08-31 10:12:07 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,7 @@ exports.run = async (bot, msg, args) => {
                 embed: {
                     color: "#1167b1",
                     title: "Fun commands",
-                    description: '``meme`` Gets a random meme.\n``dadjoke`` Gets a random dad joke.\n``gayrate`` See how gay is someone\n``hug`` Hug a user!\n``punch`` Punch a user!\n``topic`` Gives a chat topic\n``urban`` Searches something on urban dictionary.\n``kymeme`` Searches something on Know Your Meme.',
+                    description: '``meme`` Gets a random meme.\n``dadjoke`` Gets a random dad joke.\n``aviationmeme`` Gets a random aviation meme.\n``gayrate`` See how gay is someone\n``hug`` Hug a user!\n``punch`` Punch a user!\n``topic`` Gives a chat topic\n``urban`` Searches something on urban dictionary.\n``kymeme`` Searches something on Know Your Meme.',
                     footer: {
                         iconURL: "https://i.imgur.com/XiiqQWn.png",
                         text: "This server's prefix is " + prefix
