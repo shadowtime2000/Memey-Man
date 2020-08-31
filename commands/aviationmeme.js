effc9cd8eb8e627fa3641188148db1fc9ee08567
calvinjung <myadiuio@gmail.com>
2020-08-31 09:28:41 +0900

    Update aviationmeme.js

--- commands/aviationmeme.js
+++ commands/aviationmeme.js
@@ -1,36 +1,25 @@
 const Discord = require("discord.js")
+const randomPuppy = require('random-puppy')
 const request = require('request');
 const cheerio = require('cheerio');
-const randomPuppy = require('random-puppy')
 exports.run = (bot, msg, args) => {
 
-    let title
     let meme
     
     randomPuppy("aviationmemes")
     .then(url => {
-
         meme = url
-
-        request(meme, (error, resp, body) => {
-            if(error) {
-                console.log(error)
-            }
-            let $ = cheerio.load(body);
-            let $title = $('.post-title').text();
-
-            title = $title
-
-        });
-
     })
 
-    const aviationmeme = new Discord.MessageEmbed()
-        .setColor("#7cfc00")
-        .setTitle(title)
-        .setImage(meme)
-        
-    msg.channel.send(aviationmeme)
-    
+    request(meme, (error, resp, body) => {
+        if(error) {
+            console.log(error)
+        }
+        let $ = cheerio.load(body);
+        let $title = $('.post-title').text();
+
+        console.log($title)
+
+    });
 
 };
