8d1b58aaa40bdba27e9d1abe85b9473acff12050
calvinjung <myadiuio@gmail.com>
2020-08-31 09:48:33 +0900

    Update aviationmeme.js

--- commands/aviationmeme.js
+++ commands/aviationmeme.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const randomPuppy = require('random-puppy')
 const request = require('request');
 const cheerio = require('cheerio');
-exports.run = (bot, msg, args) => {
+exports.run = async (bot, msg, args) => {
 
     var res = await randomPuppy("aviationmemes")
     var url = res.split(".")[0] + "." + res.split(".")[1]
