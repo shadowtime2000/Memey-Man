01b3618cefa3eaa831447fbf625e65d00c7f0cb3
calvinjung <myadiuio@gmail.com>
2020-09-01 15:02:27 +0900

    Update uwu.js

--- commands/uwu.js
+++ commands/uwu.js
@@ -1,14 +1,13 @@
 const Discord = require("discord.js")
 const uwufy = require('uwufy')
 exports.run = (bot, msg, args) => {
-    if(msg.mentions.users.first || msg.mentions.roles.first) {
-        const noping = new Discord.MessageEmbed()
-            .setColor('#FF665B')
-            .setTitle("User/role ping detected")
-            .setDescription("Don't try to ping users and roles!")
+    const noping = new Discord.MessageEmbed()
+        .setColor('#FF665B')
+        .setTitle("User/role ping detected")
+        .setDescription("Don't try to ping users and roles!")
 
-        return msg.channel.send(noping)
-    }
+    if(msg.mentions.users.first) return msg.channel.send(noping)
+    if(msg.mentions.roles.first) return msg.channel.send(noping)
 
     const args1 = msg.content.split(' ').slice(1); 
     const text = args1.join(' ');  
