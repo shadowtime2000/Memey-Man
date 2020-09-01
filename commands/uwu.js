c6c23a7e7d5fe6b676770c5497fb178f8f659af7
calvinjung <myadiuio@gmail.com>
2020-09-01 14:54:59 +0900

    uwu

---
+++ commands/uwu.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+const uwufy = require('uwufy')
+exports.run = (bot, msg, args) => {
+    if(msg.mentions.users.first || msg.mentions.roles.first) {
+        const noping = new Discord.MessageEmbed()
+            .setColor('#FF665B')
+            .setTitle("User/role ping detected")
+            .setDescription("Don't try to ping users and roles!")
+
+        return msg.channel.send(noping)
+    }
+
+    const args1 = msg.content.split(' ').slice(1); 
+    const text = args1.join(' ');  
+
+    msg.channel.send(uwufy(text))
+};
