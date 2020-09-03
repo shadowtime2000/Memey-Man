97ad04ef20cde66b929c50ddfab924320e074360
unknown <myadiuio@gmail.com>
2020-09-03 10:35:59 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -36,7 +36,7 @@ exports.run = async (bot, msg, args) => {
                 embed: {
                     color: "#1167b1",
                     title: "Search commands",
-                    description: '``wiki`` Searches something on Wikipedia.\n``urban`` Searches something on urban dictionary.\n``kymeme`` Searches something on Know Your Meme.',
+                    description: '``wiki`` Searches something on Wikipedia.``imgsearch`` Searches image on Google.\n``urban`` Searches something on urban dictionary.\n``kymeme`` Searches something on Know Your Meme.',
                     footer: {
                         iconURL: "https://i.imgur.com/XiiqQWn.png",
                         text: "This server's prefix is " + prefix
@@ -48,7 +48,7 @@ exports.run = async (bot, msg, args) => {
                 embed: {
                     color: "#1167b1",
                     title: "Image commands",
-                    description: '``imgsearch`` Searches image.\n``dog`` Gives a dog image.\n``fox`` Gives a fox image.\n``draw`` Creates an image.\n``amiajoke`` Am I a joke to you image with your pfp.\n``pogchamp`` Pogchamp image with your pfp.\n``nou`` No u image with your pfp.\n``russia`` Mother Russia!\n``hackerman`` Hackerman image with your pfp.',
+                    description: '``dog`` Gives a dog image.\n``fox`` Gives a fox image.\n``draw`` Creates an image.\n``amiajoke`` Am I a joke to you image with your pfp.\n``pogchamp`` Pogchamp image with your pfp.\n``nou`` No u image with your pfp.\n``russia`` Mother Russia!\n``hackerman`` Hackerman image with your pfp.',
                     footer: {
                         iconURL: "https://i.imgur.com/XiiqQWn.png",
                         text: "This server's prefix is " + prefix
