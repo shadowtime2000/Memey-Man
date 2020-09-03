2c3b6044d416f77afdbbc08f15a05982f03640a9
unknown <myadiuio@gmail.com>
2020-09-03 14:10:26 +0900

    Create supreme.js

---
+++ commands/supreme.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+exports.run = (bot, msg, args) => {
+
+    var arguments = msg.content.split(" ").slice(1)
+    var text = arguments.join(" ")
+
+    const inv = new Discord.MessageEmbed()
+        .setColor("#FF665B")
+        .setTitle("Invalid argument")
+        .setDescription(prefix + "supreme [text]")
+        .setFooter("You didn't provide the text.")
+
+    if(!text) return msg.channel.send(inv)
+
+    msg.channel.send({file: `https://api.alexflipnote.dev/supreme?text=${text}`})
+    
+};
