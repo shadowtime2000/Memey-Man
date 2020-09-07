a0175bf04c5fc82a2314a602121dc6389e7c0073
unknown <myadiuio@gmail.com>
2020-09-07 16:49:33 +0900

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -5,13 +5,13 @@ exports.run = (bot, msg, args) => {
     const infoEmbed = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('**Bot info**')
-	.setThumbnail('https://i.imgur.com/XiiqQWn.png')
-	.addFields(
-            { name: 'Bot name', value: 'Memey Man'  },
-            { name: 'Developer', value: 'RedTea#8520', inline = true },
-            { name: 'Bot prefix', value: prefix, inline = true},
-            { name: 'Server count', value: bot.guilds.cache.size },
-            { name: 'USer count', value: bot.users.cache.size, inline = true},
+        .setThumbnail('https://i.imgur.com/XiiqQWn.png')
+        .addFields(
+                { name: 'Bot name', value: 'Memey Man'  },
+                { name: 'Developer', value: 'RedTea#8520', inline = true },
+                { name: 'Bot prefix', value: prefix, inline = true},
+                { name: 'Server count', value: bot.guilds.cache.size },
+                { name: 'USer count', value: bot.users.cache.size, inline = true},
         )
         .setFooter('Type ' + prefix + 'help to get help!')
 
