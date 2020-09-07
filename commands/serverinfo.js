6b17bc96ecc5f4c7a3448d6572451f81e1455d58
unknown <myadiuio@gmail.com>
2020-09-07 17:15:10 +0900

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -17,7 +17,11 @@ exports.run = (bot, msg, args) => {
             { name: 'Member count', value: msg.guild.memberCount, inline: true},
             { name: 'Role count', value: msg.guild.roles.cache.size, inline: true },
             { name: 'Emoji count', value: msg.guild.emojis.cache.size, inline: true },
-            { name: 'Server ID', value: msg.guild.id, inline: true }
+        )
+        .addFields(
+            { name: 'Server ID', value: msg.guild.id, inline: true },
+            { name: 'Server region', value: msg.guild.region, inline: true },
+            { name: 'Verification level', value: msg.guild.verificationLevel, inline: true }
         )
         .setTimestamp()
 
