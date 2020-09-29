1ec1aa55e3eb5c0f1a0e8706cc73f1da06421aa5
unknown <myadiuio@gmail.com>
2020-09-29 12:59:40 +0900

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -2,18 +2,57 @@ const Discord = require('discord.js');
 const fetch = require('node-fetch');
 exports.run = (bot, msg, args) => {
 
-    fetch('https://meme-api.herokuapp.com/gimme')
-    .then(res => res.json())
-    .then(json => {
+    function checkURL(url) {
+        return(String(url).match(/\.(jpeg|jpg|gif|png)$/) != null);
+    }
 
-        const memeembed = new Discord.MessageEmbed()
-            .setColor('#7cfc00')
-            .setTitle(json.title)
-            .setImage(json.url)
-            .setFooter(`Subreddit: r/${json.subreddit}`)   
+    if (msg.channel.nsfw == false) {
 
-        msg.channel.send(memeembed)   
+        async function fetchRedditData() {
 
-    });
+            const res = await fetch("https://www.reddit.com/r/aviationmemes/random.json");
+            const data = await res.json();
+        
+            const url = data[0].data.children[0].data.url
+            const title = data[0].data.children[0].data.title
+            const author = data[0].data.children[0].data.author
+    
+            if(checkURL(url) == false) {
+    
+                await fetchRedditData()
+    
+            } else {
+    
+                const meme = new Discord.MessageEmbed()
+                    .setColor("#7cfc00")
+                    .setTitle(title)
+                    .setImage(url)
+                    .setFooter(`Meme by u/${author}`)
+    
+                return msg.channel.send(meme)
+    
+            }
+    
+        }
+        
+        fetchRedditData()
+        
+    } else {
+
+        fetch('https://meme-api.herokuapp.com/gimme')
+        .then(res => res.json())
+        .then(json => {
+
+            const memeembed = new Discord.MessageEmbed()
+                .setColor('#7cfc00')
+                .setTitle(json.title)
+                .setImage(json.url)
+                .setFooter(`NSFW channel detected | Subreddit: r/${json.subreddit}`)   
+
+            msg.channel.send(memeembed)   
+
+        });
+
+    }
 
 };
